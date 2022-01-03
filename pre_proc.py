d5b7b1afede054daa3fc15299e9bce04b3248c46
WeijieChen2017 <weijiechen1994@gmail.com>
2022-01-03 17:43:33 -0600

    Good morning!

--- pre_proc.py
+++ pre_proc.py
@@ -1,17 +1,25 @@
+import os
+import glob
 import time
 import numpy as np
+import nibabel as nib
 
 pre_proc_dict = {}
 
-pre_proc_dict["dir_x_orig"] = 
-pre_proc_dict["dir_y_orig"] = 
-pre_proc_dict["dir_x_syn"] = 
-pre_proc_dict["dir_y_syn"] = 
-pre_proc_dict["range_norm_x"] = 
-pre_proc_dict["range_norm_y"] = 
-pre_proc_dict["is_seg_x"] = 
-pre_proc_dict["is_seg_y"] = 
+pre_proc_dict["dir_orig"] = "./data_dir/MR2CT/"
+pre_proc_dict["name_orig"] = "CT__MLAC_*_MNI.nii.gz"
+pre_proc_dict["dir_syn"] = "./data_dir/norm_MR/"
+pre_proc_dict["is_seg"] = False
+pre_proc_dict["range_seg"] = [[0, 2000]]
 pre_proc_dict["time_stamp"] = time.strftime("%Y-%m-%d_%H:%M:%S", time.localtime())
 
-np.save("./log_dir/log_pre_proc_"+pre_proc_dict["time_stamp"]+".npy", )
-        
+# np.save("./log_dir/log_pre_proc_"+pre_proc_dict["time_stamp"]+".npy", )
+
+file_list = sorted(glob.glob(pre_proc_dict["dir_orig"]+pre_proc_dict["name_orig"]))
+for file_path  in file_list:
+    print("-"*60)
+    print(file_path)
+    file_nifty = nib.load(file_path)
+    file_data = nib.load_fdata(file_nifty)
+    print(np.amax(file_data), np.amin(file_data))
+
