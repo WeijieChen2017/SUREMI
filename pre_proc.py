32362cc33888a4354361e57244a2b52be85e1432
WeijieChen2017 <weijiechen1994@gmail.com>
2022-01-03 17:51:50 -0600

    Good morning!

--- pre_proc.py
+++ pre_proc.py
@@ -20,6 +20,6 @@ for file_path  in file_list:
     print("-"*60)
     print(file_path)
     file_nifty = nib.load(file_path)
-    file_data = nib.load_fdata(file_nifty)
+    file_data = file_nifty.load_fdata()
     print(np.amax(file_data), np.amin(file_data))
 
