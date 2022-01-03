fa2333345ef66ffb88befcff6e8d1c1417ede41e
WeijieChen2017 <weijiechen1994@gmail.com>
2022-01-03 00:05:03 -0600

    Good morning!

--- pre_proc.py
+++ pre_proc.py
@@ -1,1 +1,17 @@
-pre
+import time
+import numpy as np
+
+pre_proc_dict = {}
+
+pre_proc_dict["dir_x_orig"] = 
+pre_proc_dict["dir_y_orig"] = 
+pre_proc_dict["dir_x_syn"] = 
+pre_proc_dict["dir_y_syn"] = 
+pre_proc_dict["range_norm_x"] = 
+pre_proc_dict["range_norm_y"] = 
+pre_proc_dict["is_seg_x"] = 
+pre_proc_dict["is_seg_y"] = 
+pre_proc_dict["time_stamp"] = time.strftime("%Y-%m-%d_%H:%M:%S", time.localtime())
+
+np.save("./log_dir/log_pre_proc_"+pre_proc_dict["time_stamp"]+".npy", )
+        
