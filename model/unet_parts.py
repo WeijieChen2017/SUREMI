0d226b455d89ba96b7f2cff19f2f363bc0e20388
WeijieChen2017 <weijiechen1994@gmail.com>
2022-01-04 04:03:40 -0600

    Good morning!

--- model/unet_parts.py
+++ model/unet_parts.py
@@ -115,7 +115,7 @@ class OutConv_seg(nn.Module):
     def __init__(self, in_channels, out_channels):
         super(OutConv_seg, self).__init__()
         self.conv = nn.Conv2d(in_channels, in_channels, kernel_size=1)
-        # self.sigmoid = nn.Sigmoid()
+        self.sigmoid = nn.Sigmoid()
         self.conv1 = nn.Conv2d(in_channels, out_channels, kernel_size=1)
 
     def forward(self, x):
