20a8544b8988162f1ec192e59f5f93ed9da138ce
WeijieChen2017 <weijiechen1994@gmail.com>
2022-01-04 03:51:48 -0600

    Good morning!

--- train.py
+++ train.py
@@ -18,21 +18,21 @@ from model import UNet
 
 train_dict = {}
 train_dict["time_stamp"] = time.strftime("%Y-%m-%d_%H:%M:%S", time.localtime())
-train_dict["project_name"] = "seg_to_CT"
+train_dict["project_name"] = "MR_to_seg"
 train_dict["save_folder"] = "./project_dir/"+train_dict["project_name"]+"/"
 train_dict["seed"] = 426
-train_dict["input_channel"] = 1
-train_dict["output_channel"] = 1
+train_dict["input_channel"] = 3
+train_dict["output_channel"] = 3
 train_dict["gpu_ids"] = [5]
 train_dict["epochs"] = 50
 train_dict["batch"] = 10
 train_dict["dropout"] = 0
 train_dict["model_term"] = "UNet"
 
-train_dict["folder_X"] = "./data_dir/seg_CT/"
-train_dict["folder_Y"] = "./data_dir/norm_CT/"
-train_dict["val_ratio"] = 0.2
-train_dict["test_ratio"] = 0.1
+train_dict["folder_X"] = "./data_dir/norm_MR/regular/"
+train_dict["folder_Y"] = "./data_dir/seg_CT/"
+train_dict["val_ratio"] = 0.3
+train_dict["test_ratio"] = 0.2
 
 train_dict["loss_term"] = "SmoothL1Loss"
 train_dict["optimizer"] = "AdamW"
@@ -58,7 +58,6 @@ config.loss_term = train_dict["loss_term"]
 config.opt_lr = train_dict["opt_lr"]
 config.opt_weight_decay = train_dict["opt_weight_decay"]
 
-# np.save(train_dict["save_folder"]+"conf.npy", config)
 np.save(train_dict["save_folder"]+"dict.npy", train_dict)
 
 
@@ -111,241 +110,109 @@ np.save(train_dict["save_folder"]+"data_division.npy", data_division_dict)
 
 # ==================== training ====================
 
-train_loss = np.zeros((train_dict["epochs"]))
-epoch_loss_train = np.zeros((len(train_list_X)))
-epoch_loss_val = np.zeros((len(val_list_X)))
-epoch_loss_test = np.zeros((len(test_list_X)))
 best_val_loss = 1e6
-case_loss = None
-best_model = None
 wandb.watch(model)
 
 for idx_epoch in range(train_dict["epochs"]):
     print("~~~~~~Epoch[{:03d}]~~~~~~".format(idx_epoch+1))
 
-    # ====================================>train<====================================
-    model.train()
-    random.shuffle(train_list_X)
-    for cnt_file, file_path in enumerate(train_list_X):
-        # cube_x_path = file_path.replace("seg", "norm")
-        cube_y_path = file_path.replace("seg", "norm")
-        cube_x_path = file_path
-        # cube_y_path = file_path
-        print("--->",cube_x_path,"<---", end="")
-        cube_x_data = nib.load(cube_x_path).get_fdata()
-        cube_y_data = nib.load(cube_y_path).get_fdata()
-        len_z = cube_x_data.shape[2]
-        case_loss = np.zeros((len_z//train_dict["batch"]))
-        input_list = list(range(len_z))
-        random.shuffle(input_list)
+    package_train = [train_list_X, True, False, "train"]
+    package_val = [val_list_X, False, True, "val"]
+    package_test = [test_list_X, False, False, "test"]
 
-        for idx_iter in range(len_z//train_dict["batch"]):
+    for package in [package_train, package_val, package_test]:
 
-            batch_x = np.zeros((train_dict["batch"], train_dict["input_channel"], cube_x_data.shape[0], cube_x_data.shape[1]))
-            batch_y = np.zeros((train_dict["batch"], train_dict["output_channel"], cube_y_data.shape[0], cube_y_data.shape[1]))
+        file_list = package[0]
+        isTrain = package[1]
+        isVal = package[2]
+        iter_tag = package[3]
 
-            for idx_batch in range(train_dict["batch"]):
-                z_center = input_list[idx_iter*train_dict["batch"]+idx_batch]
-                z_before = z_center - 1 if z_center > 0 else 0
-                z_after = z_center + 1 if z_center < len_z-1 else len_z-1
+        if isTrain:
+            model.train()
+        else:
+            model.eval()
 
-                if train_dict["input_channel"] == 3:
-                    batch_x[idx_batch, 1, :, :] = cube_x_data[:, :, z_center]
-                    batch_x[idx_batch, 0, :, :] = cube_x_data[:, :, z_before]
-                    batch_x[idx_batch, 2, :, :] = cube_x_data[:, :, z_after]
-                if train_dict["input_channel"] == 1:
-                    batch_x[idx_batch, 0, :, :] = cube_x_data[:, :, z_center]
-                if train_dict["output_channel"] == 3:
-                    batch_y[idx_batch, 0, :, :] = cube_y_data[:, :, z_before]
-                    batch_y[idx_batch, 1, :, :] = cube_y_data[:, :, z_center]
-                    batch_y[idx_batch, 2, :, :] = cube_y_data[:, :, z_after]
-                if train_dict["output_channel"] == 1:
-                    batch_y[idx_batch, 0, :, :] = cube_y_data[:, :, z_center]
-
-            batch_x = torch.from_numpy(batch_x).float().to(device)
-            batch_y = torch.from_numpy(batch_y).float().to(device)
-
-            optimizer.zero_grad()
-            y_hat = model(batch_x)
-            loss = criterion(y_hat, batch_y)
-            loss.backward()
-            optimizer.step()
-
-            case_loss[idx_iter] = loss.item()
-            case_loss[idx_iter] = loss.item()
-        
-        case_name = os.path.basename(cube_x_path)[5:8]
-        np.save(train_dict["save_folder"]+"npy/Epoch[{:03d}]_Case[{}]_t_x.npy".format(idx_epoch+1, case_name), batch_x.cpu().detach().numpy())
-        np.save(train_dict["save_folder"]+"npy/Epoch[{:03d}]_Case[{}]_t_y.npy".format(idx_epoch+1, case_name), batch_y.cpu().detach().numpy())
-        np.save(train_dict["save_folder"]+"npy/Epoch[{:03d}]_Case[{}]_t_z.npy".format(idx_epoch+1, case_name), y_hat.cpu().detach().numpy())
-
-        # after training one case
-        loss_mean = np.mean(case_loss)
-        loss_std = np.std(case_loss)
-        print("===> Epoch[{:03d}]-Case[{:03d}]: ".format(idx_epoch+1, cnt_file+1), end='')
-        print("Loss mean: {:.6} Loss std: {:.6}".format(loss_mean, loss_std))
-        epoch_loss_train[cnt_file] = loss_mean
-
-    # after training all cases
-    loss_mean = np.mean(epoch_loss_train)
-    loss_std = np.std(epoch_loss_train)
-    print("===> Epoch[{}]: ".format(idx_epoch+1), end='')
-    print("Loss mean: {:.6} Loss std: {:.6}".format(loss_mean, loss_std))
-    wandb.log({"train_loss": loss_mean})
-    np.save(train_dict["save_folder"]+"loss/epoch_loss_t_{:03d}.npy".format(idx_epoch+1), epoch_loss_train)
-    train_loss[idx_epoch] = loss_mean
-    torch.cuda.empty_cache()
-    # ====================================>train<====================================
-
-    # ====================================>val<====================================
-    model.eval()
-    random.shuffle(val_list_X)
-    for cnt_file, file_path in enumerate(val_list_X):
-
-        # eval
-        # cube_x_path = file_path.replace("seg", "norm")
-        cube_y_path = file_path.replace("seg", "norm")
-        cube_x_path = file_path
-        # cube_y_path = file_path
-        print("--->",cube_x_path,"<---", end="")
-        cube_x_data = nib.load(cube_x_path).get_fdata()
-        cube_y_data = nib.load(cube_y_path).get_fdata()
-        len_z = cube_x_data.shape[2]
-        case_loss = np.zeros((len_z//train_dict["batch"]))
-        input_list = list(range(len_z))
-        random.shuffle(input_list)
-
-        for idx_iter in range(len_z//train_dict["batch"]):
-
-            batch_x = np.zeros((train_dict["batch"], train_dict["input_channel"], cube_x_data.shape[0], cube_x_data.shape[1]))
-            batch_y = np.zeros((train_dict["batch"], train_dict["input_channel"], cube_y_data.shape[0], cube_y_data.shape[1]))
-
-            for idx_batch in range(train_dict["batch"]):
-                z_center = input_list[idx_iter*train_dict["batch"]+idx_batch]
-                z_before = z_center - 1 if z_center > 0 else 0
-                z_after = z_center + 1 if z_center < len_z-1 else len_z-1
-
-                if train_dict["input_channel"] == 3:
-                    batch_x[idx_batch, 1, :, :] = cube_x_data[:, :, z_center]
-                    batch_x[idx_batch, 0, :, :] = cube_x_data[:, :, z_before]
-                    batch_x[idx_batch, 2, :, :] = cube_x_data[:, :, z_after]
-                if train_dict["input_channel"] == 1:
-                    batch_x[idx_batch, 0, :, :] = cube_x_data[:, :, z_center]
-                if train_dict["output_channel"] == 3:
-                    batch_y[idx_batch, 0, :, :] = cube_y_data[:, :, z_before]
-                    batch_y[idx_batch, 1, :, :] = cube_y_data[:, :, z_center]
-                    batch_y[idx_batch, 2, :, :] = cube_y_data[:, :, z_after]
-                if train_dict["output_channel"] == 1:
-                    batch_y[idx_batch, 0, :, :] = cube_y_data[:, :, z_center]
+        random.shuffle(file_list)
+        epoch_loss = np.zeros((len(file_list)))
+        for cnt_file, file_path in enumerate(file_list):
             
-            batch_x = torch.from_numpy(batch_x).float().to(device)
-            batch_y = torch.from_numpy(batch_y).float().to(device)
+            cube_x_path = file_path
+            file_name = os.path.basename(file_path)
+            cube_y1_path = train_dict["folder_Y"] + "/air/" + file_name
+            cube_y2_path = train_dict["folder_Y"] + "/bone/" + file_name
+            cube_y3_path = train_dict["folder_Y"] + "/soft_tissue/" + file_name
+            print("--->",cube_x_path,"<---", end="")
+            cube_x_data = nib.load(cube_x_path).get_fdata()
+            cube_y1_data = nib.load(cube_y1_path).get_fdata()
+            cube_y2_data = nib.load(cube_y2_path).get_fdata()
+            cube_y3_data = nib.load(cube_y3_path).get_fdata()
+            len_z = cube_x_data.shape[2]
+            case_loss = np.zeros((len_z//train_dict["batch"]))
+            input_list = list(range(len_z))
+            random.shuffle(input_list)
+
+            for idx_iter in range(len_z//train_dict["batch"]):
+
+                batch_x = np.zeros((train_dict["batch"], train_dict["input_channel"], cube_x_data.shape[0], cube_x_data.shape[1]))
+                batch_y = np.zeros((train_dict["batch"], train_dict["output_channel"], cube_y_data.shape[0], cube_y_data.shape[1]))
+
+                for idx_batch in range(train_dict["batch"]):
+                    z_center = input_list[idx_iter*train_dict["batch"]+idx_batch]
+                    z_before = z_center - 1 if z_center > 0 else 0
+                    z_after = z_center + 1 if z_center < len_z-1 else len_z-1
+
+                    if train_dict["input_channel"] == 3:
+                        batch_x[idx_batch, 1, :, :] = cube_x_data[:, :, z_center]
+                        batch_x[idx_batch, 0, :, :] = cube_x_data[:, :, z_before]
+                        batch_x[idx_batch, 2, :, :] = cube_x_data[:, :, z_after]
+                    if train_dict["input_channel"] == 1:
+                        batch_x[idx_batch, 0, :, :] = cube_x_data[:, :, z_center]
+                    if train_dict["output_channel"] == 3:
+                        batch_y[idx_batch, 0, :, :] = cube_y1_data[:, :, z_before]
+                        batch_y[idx_batch, 1, :, :] = cube_y2_data[:, :, z_center]
+                        batch_y[idx_batch, 2, :, :] = cube_y3_data[:, :, z_after]
+                    if train_dict["output_channel"] == 1:
+                        batch_y[idx_batch, 0, :, :] = cube_y_data[:, :, z_center]
+
+                batch_x = torch.from_numpy(batch_x).float().to(device)
+                batch_y = torch.from_numpy(batch_y).float().to(device)
+
+                optimizer.zero_grad()
+                y_hat = model(batch_x)
+                loss = criterion(y_hat, batch_y)
+                if isTrain:
+                    loss.backward()
+                    optimizer.step()
+
+                case_loss[idx_iter] = loss.item()
+                case_loss[idx_iter] = loss.item()
             
-            y_hat = model(batch_x)
-            loss = criterion(y_hat, batch_y)
-            case_loss[idx_iter] = loss.item()
-        
-        # save one progress shot
-        case_name = os.path.basename(cube_x_path)[5:8]
-        np.save(train_dict["save_folder"]+"npy/Epoch[{:03d}]_Case[{}]_v_x.npy".format(idx_epoch+1, case_name), batch_x.cpu().detach().numpy())
-        np.save(train_dict["save_folder"]+"npy/Epoch[{:03d}]_Case[{}]_v_y.npy".format(idx_epoch+1, case_name), batch_y.cpu().detach().numpy())
-        np.save(train_dict["save_folder"]+"npy/Epoch[{:03d}]_Case[{}]_v_z.npy".format(idx_epoch+1, case_name), y_hat.cpu().detach().numpy())
+            case_name = os.path.basename(cube_x_path)[5:8]
+            if not isTrain:
+                np.save(train_dict["save_folder"]+"npy/Epoch[{:03d}]_Case[{}]_"+iter_tag+"_x.npy".format(idx_epoch+1, case_name), batch_x.cpu().detach().numpy())
+                np.save(train_dict["save_folder"]+"npy/Epoch[{:03d}]_Case[{}]_"+iter_tag+"_y.npy".format(idx_epoch+1, case_name), batch_y.cpu().detach().numpy())
+                np.save(train_dict["save_folder"]+"npy/Epoch[{:03d}]_Case[{}]_"+iter_tag+"_z.npy".format(idx_epoch+1, case_name), y_hat.cpu().detach().numpy())
 
-        # after training one case
-        loss_mean = np.mean(case_loss)
-        loss_std = np.std(case_loss)
-        print("===> Epoch[{:03d}]-Val-Case[{:03d}]: ".format(idx_epoch+1, cnt_file+1), end='')
+            # after training one case
+            loss_mean = np.mean(case_loss)
+            loss_std = np.std(case_loss)
+            print("===> Epoch[{:03d}]-Case[{:03d}]: ".format(idx_epoch+1, cnt_file+1), end='')
+            print("Loss mean: {:.6} Loss std: {:.6}".format(loss_mean, loss_std))
+            epoch_loss[cnt_file] = loss_mean
+
+        # after training all cases
+        loss_mean = np.mean(epoch_loss)
+        loss_std = np.std(epoch_loss)
+        print("===> Epoch[{}]: ".format(idx_epoch+1), end='')
         print("Loss mean: {:.6} Loss std: {:.6}".format(loss_mean, loss_std))
-        epoch_loss_val[cnt_file] = loss_mean
+        wandb.log({iter_tag+"_loss": loss_mean})
+        np.save(train_dict["save_folder"]+"loss/epoch_loss_"+iter_tag+"_{:03d}.npy".format(idx_epoch+1), epoch_loss)
 
-    loss_mean = np.mean(epoch_loss_val)
-    loss_std = np.std(epoch_loss_val)
-    print("===> Epoch[{:03d}]-Val: ".format(idx_epoch+1), end='')
-    print("Loss mean: {:.6} Loss std: {:.6}".format(loss_mean, loss_std))
-    wandb.log({"val_loss": loss_mean})
-    np.save(train_dict["save_folder"]+"loss/epoch_loss_v_{:03d}.npy".format(idx_epoch+1), epoch_loss_val)
-    if loss_mean < best_val_loss:
-        # save the best model
-        best_model = model
-        torch.save(model, train_dict["save_folder"]+"model_best_{:03d}.pth".format(idx_epoch+1))
-        print("Checkpoint saved at Epoch {:03d}".format(idx_epoch+1))
-        best_val_loss = loss_mean
-    torch.cuda.empty_cache()
-    # ====================================>val<====================================
+        if isVal:
+            if loss_mean < best_val_loss:
+            # save the best model
+            torch.save(model, train_dict["save_folder"]+"model_best_{:03d}.pth".format(idx_epoch+1))
+            print("Checkpoint saved at Epoch {:03d}".format(idx_epoch+1))
+            best_val_loss = loss_mean
 
-loss_mean = np.mean(train_loss)
-loss_std = np.std(train_loss)
-print("===>Training finished: ", end='')
-print("Loss mean: {:.6} Loss std: {:.6}".format(loss_mean, loss_std))
-
-
-# ====================================>test<====================================
-
-model = best_model
-model.eval()
-random.shuffle(test_list_X)
-for cnt_file, file_path in enumerate(test_list_X):
-
-    # eval
-    # cube_x_path = file_path.replace("seg", "norm")
-    cube_y_path = file_path.replace("seg", "norm")
-    cube_x_path = file_path
-    # cube_y_path = file_path
-    print("--->",cube_x_path,"<---", end="")
-    cube_x_data = nib.load(cube_x_path).get_fdata()
-    cube_y_data = nib.load(cube_y_path).get_fdata()
-    len_z = cube_x_data.shape[2]
-    case_loss = np.zeros((len_z//train_dict["batch"]))
-    input_list = list(range(len_z))
-    random.shuffle(input_list)
-
-    for idx_iter in range(len_z//train_dict["batch"]):
-
-        batch_x = np.zeros((train_dict["batch"], train_dict["input_channel"], cube_x_data.shape[0], cube_x_data.shape[1]))
-        batch_y = np.zeros((train_dict["batch"], train_dict["input_channel"], cube_y_data.shape[0], cube_y_data.shape[1]))
-
-        for idx_batch in range(train_dict["batch"]):
-            z_center = input_list[idx_iter*train_dict["batch"]+idx_batch]
-            z_before = z_center - 1 if z_center > 0 else 0
-            z_after = z_center + 1 if z_center < len_z-1 else len_z-1
-
-            if train_dict["input_channel"] == 3:
-                batch_x[idx_batch, 1, :, :] = cube_x_data[:, :, z_center]
-                batch_x[idx_batch, 0, :, :] = cube_x_data[:, :, z_before]
-                batch_x[idx_batch, 2, :, :] = cube_x_data[:, :, z_after]
-            if train_dict["input_channel"] == 1:
-                batch_x[idx_batch, 0, :, :] = cube_x_data[:, :, z_center]
-            if train_dict["output_channel"] == 3:
-                batch_y[idx_batch, 0, :, :] = cube_y_data[:, :, z_before]
-                batch_y[idx_batch, 1, :, :] = cube_y_data[:, :, z_center]
-                batch_y[idx_batch, 2, :, :] = cube_y_data[:, :, z_after]
-            if train_dict["output_channel"] == 1:
-                batch_y[idx_batch, 0, :, :] = cube_y_data[:, :, z_center]
-        
-        batch_x = torch.from_numpy(batch_x).float().to(device)
-        batch_y = torch.from_numpy(batch_y).float().to(device)
-        
-        y_hat = model(batch_x)
-        loss = criterion(y_hat, batch_y)
-        case_loss[idx_iter] = loss.item()
-    
-    # save one progress shot
-    case_name = os.path.basename(cube_x_path)[5:8]
-    np.save(train_dict["save_folder"]+"npy/Epoch[{:03d}]_Case[{}]_e_x.npy".format(idx_epoch+1, case_name), batch_x.cpu().detach().numpy())
-    np.save(train_dict["save_folder"]+"npy/Epoch[{:03d}]_Case[{}]_e_y.npy".format(idx_epoch+1, case_name), batch_y.cpu().detach().numpy())
-    np.save(train_dict["save_folder"]+"npy/Epoch[{:03d}]_Case[{}]_e_z.npy".format(idx_epoch+1, case_name), y_hat.cpu().detach().numpy())
-
-    # after training one case
-    loss_mean = np.mean(case_loss)
-    loss_std = np.std(case_loss)
-    print("===> Epoch[{:03d}]-Eval-Case[{:03d}]: ".format(idx_epoch+1, cnt_file+1), end='')
-    print("Loss mean: {:.6} Loss std: {:.6}".format(loss_mean, loss_std))
-    epoch_loss_test[cnt_file] = loss_mean
-
-loss_mean = np.mean(epoch_loss_test)
-loss_std = np.std(epoch_loss_test)
-print("===> Epoch[{:03d}]-Eval: ".format(idx_epoch+1), end='')
-print("Loss mean: {:.6} Loss std: {:.6}".format(loss_mean, loss_std))
-np.save(train_dict["save_folder"]+"loss/epoch_loss_e_{:03d}.npy".format(idx_epoch+1), epoch_loss_test)
-
-wandb.log({"test_loss": loss_mean})
+        torch.cuda.empty_cache()
