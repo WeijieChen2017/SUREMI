c1aee1be53355a13159d156c9aab52afaee093c1
WeijieChen2017 <weijiechen1994@gmail.com>
2022-01-03 22:04:01 -0600

    Good morning!

--- pre_proc.py
+++ pre_proc.py
@@ -5,16 +5,67 @@ import time
 import numpy as np
 import nibabel as nib
 
+# ------------------------------CT------------------------------
+
+# pre_proc_dict = {}
+
+# pre_proc_dict["dir_orig"] = "./data_dir/MR2CT/"
+# pre_proc_dict["name_orig"] = "CT__MLAC_*_MNI.nii.gz"
+# pre_proc_dict["dir_syn"] = "./data_dir/seg_CT/"
+# pre_proc_dict["is_seg"] = True
+# pre_proc_dict["attr_seg"] = ["air", "soft tissue", "bone"]
+# pre_proc_dict["range_seg"] = [[-1024, -500], [-500, 500], [500, 3000]]
+# pre_proc_dict["note"] = []
+# pre_proc_dict["time_stamp"] = time.strftime("%Y-%m-%d_%H:%M:%S", time.localtime())
+
+# for CT
+# file_list = sorted(glob.glob(pre_proc_dict["dir_orig"]+pre_proc_dict["name_orig"]))
+# for file_path  in file_list:
+#     print("-"*60)
+#     print(file_path)
+#     file_nifty = nib.load(file_path)
+#     file_data = file_nifty.get_fdata()
+#     print(np.amin(file_data), np.amax(file_data))
+
+#     pre_proc_dict["note"].append(["There are two ranges, [-1024, 2000+], [0, 3000+]"])
+#     if np.amin(file_data) >-1:
+#         file_data -= 1024
+
+#     pre_proc_dict["note"].append(["For HU values in CT, [-1024, -500] for air, [-500, 500] for soft tissue, [500, 3000] for bone"])
+#     pre_proc_dict["note"].append(["For HU values, we don't want a gap, so soft tissue should be [-200, 200], but we use [-500, 500] instead"])
+#     for idx, value_range in enumerate(pre_proc_dict["range_seg"]):
+#         value_min = value_range[0]
+#         value_max = value_range[1]
+#         value_seg = copy.deepcopy(file_data)
+#         value_seg[value_seg < value_min] = value_min
+#         value_seg[value_seg > value_max] = value_min
+#         value_seg = ( value_seg - value_min ) / (value_max - value_min)
+#         value_seg[value_seg > 0] = 1
+
+#         save_folder = pre_proc_dict["dir_syn"] + pre_proc_dict["attr_seg"][idx] + "/"
+#         if not os.path.exists(save_folder):
+#             os.makedirs(save_folder)
+#         save_name = "NORM_0"+os.path.basename(file_path)[9:11]+".nii.gz"
+#         save_nifty = nib.Nifti1Image(value_seg, file_nifty.affine, file_nifty.header)
+#         nib.save(save_nifty, save_folder+save_name)
+#         print(save_folder+save_name, " "*4, np.amin(value_seg), " "*4, np.amax(value_seg))
+
+# np.save("./log_dir/log_pre_proc_"+pre_proc_dict["time_stamp"]+".npy", pre_proc_dict)
+
+
+# ------------------------------MR------------------------------
+
 pre_proc_dict = {}
 
 pre_proc_dict["dir_orig"] = "./data_dir/MR2CT/"
-pre_proc_dict["name_orig"] = "CT__MLAC_*_MNI.nii.gz"
-pre_proc_dict["dir_syn"] = "./data_dir/seg_CT/"
-pre_proc_dict["is_seg"] = True
-pre_proc_dict["attr_seg"] = ["air", "soft tissue", "bone"]
-pre_proc_dict["range_seg"] = [[-1024, -500], [-500, 500], [500, 3000]]
+pre_proc_dict["name_orig"] = "MR__MLAC_*_MNI.nii.gz"
+pre_proc_dict["dir_syn"] = "./data_dir/norm_MR/"
+pre_proc_dict["is_seg"] = False
+pre_proc_dict["attr_seg"] = ["regular"]
+pre_proc_dict["range_seg"] = [[0, 4000]]
 pre_proc_dict["note"] = []
 pre_proc_dict["time_stamp"] = time.strftime("%Y-%m-%d_%H:%M:%S", time.localtime())
+
 
 file_list = sorted(glob.glob(pre_proc_dict["dir_orig"]+pre_proc_dict["name_orig"]))
 for file_path  in file_list:
@@ -24,12 +75,6 @@ for file_path  in file_list:
     file_data = file_nifty.get_fdata()
     print(np.amin(file_data), np.amax(file_data))
 
-    pre_proc_dict["note"].append(["There are two ranges, [-1024, 2000+], [0, 3000+]"])
-    if np.amin(file_data) >-1:
-        file_data -= 1024
-
-    pre_proc_dict["note"].append(["For HU values in CT, [-1024, -500] for air, [-500, 500] for soft tissue, [500, 3000] for bone"])
-    pre_proc_dict["note"].append(["For HU values, we don't want a gap, so soft tissue should be [-200, 200], but we use [-500, 500] instead"])
     for idx, value_range in enumerate(pre_proc_dict["range_seg"]):
         value_min = value_range[0]
         value_max = value_range[1]
@@ -37,7 +82,6 @@ for file_path  in file_list:
         value_seg[value_seg < value_min] = value_min
         value_seg[value_seg > value_max] = value_min
         value_seg = ( value_seg - value_min ) / (value_max - value_min)
-        value_seg[value_seg > 0] = 1
 
         save_folder = pre_proc_dict["dir_syn"] + pre_proc_dict["attr_seg"][idx] + "/"
         if not os.path.exists(save_folder):
@@ -48,4 +92,3 @@ for file_path  in file_list:
         print(save_folder+save_name, " "*4, np.amin(value_seg), " "*4, np.amax(value_seg))
 
 np.save("./log_dir/log_pre_proc_"+pre_proc_dict["time_stamp"]+".npy", pre_proc_dict)
-
