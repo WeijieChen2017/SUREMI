7a5107188dc02bc1c507d064efc7d1dfa50f8d57
WeijieChen2017 <weijiechen1994@gmail.com>
2022-01-10 19:23:37 -0600

    Good morning!

--- train_styleGAN.py
+++ train_styleGAN.py
@@ -84,7 +84,7 @@ gen_MR = Generator(
     num_channels=train_dict["input_channel"],
     resolution=train_dict["resolution"],
     structure=train_dict["structure"],
-    style_mixing_prob=train_dict["style_mixing_prob"]).train().to(self.device)
+    style_mixing_prob=train_dict["style_mixing_prob"]).train().to(device)
 
 gen_CT = Generator(
     latent_size=train_dict["latent_size"],
@@ -92,7 +92,7 @@ gen_CT = Generator(
     num_channels=train_dict["input_channel"],
     resolution=train_dict["resolution"],
     structure=train_dict["structure"],
-    style_mixing_prob=train_dict["style_mixing_prob"]).train().to(self.device)
+    style_mixing_prob=train_dict["style_mixing_prob"]).train().to(device)
 
 criterion_MR = nn.SmoothL1Loss()
 criterion_CT = nn.SmoothL1Loss()
