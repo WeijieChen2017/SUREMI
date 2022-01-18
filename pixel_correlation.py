041e535d1b81c073ac8ee46657383520bcaa5114
WeijieChen2017 <weijiechen1994@gmail.com>
2022-01-18 17:43:19 -0600

    Good morning!

--- pixel_correlation.py
+++ pixel_correlation.py
@@ -4,6 +4,14 @@ import time
 import numpy as np
 import nibabel as nib
 
+def bin_CT(img, n_bins=1024):
+    data_max = np.amax(img)
+    data_min = np.amin(img)
+    data_squeezed = (img-data_min)/(data_max-data_min)
+    data_extended = data_squeezed * n_bins
+    data_discrete = data_extended // 1
+    return data_discrete
+
 train_dict = {}
 train_dict["time_stamp"] = time.strftime("%Y-%m-%d_%H:%M:%S", time.localtime())
 train_dict["project_name"] = "pixel_correlation"
@@ -23,4 +31,18 @@ for cnt_file, file_path in enumerate(X_list):
     print("--->",cube_x_path,"<---")
     cube_x_data = nib.load(cube_x_path).get_fdata()
     cube_y_data = nib.load(cube_y_path).get_fdata()
-    len_z = cube_x_data.shape[2]
+    len_x, len_y, len_z = cube_x_data.shape
+
+    X_discrete = bin_CT(cube_x_data, n_bins=1024)
+    Y_discrete = bin_CT(cube_y_data, n_bins=1024)
+    
+    pixel_corr = np.zeros((1024, 1024))
+
+    for ix in range(len_x):
+    	for iy in range(len_y):
+    		for iz in range(len_z):
+    			pixel_corr[X_discrete[ix, iy, iz], Y_discrete[ix, iy, iz]] += 1
+
+    np.save(train_dict["save_folder"]+file_name+"_pix_cor.npy")
+
+
