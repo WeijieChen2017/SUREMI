d81eea11f80a6103ad373872c6c3701e6ed76517
WeijieChen2017 <weijiechen1994@gmail.com>
2022-01-19 20:46:38 -0600

    Good morning!

--- pixel_correlation.py
+++ pixel_correlation.py
@@ -3,6 +3,8 @@ import glob
 import time
 import numpy as np
 import nibabel as nib
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 def bin_CT(img, n_bins=1024):
     data_vector = np.ravel(img)
@@ -11,7 +13,7 @@ def bin_CT(img, n_bins=1024):
     data_squeezed = (data_vector-data_min)/(data_max-data_min)
     data_extended = data_squeezed * n_bins
     data_discrete = data_extended // 1
-    print(data_discrete.shape)
+#     print(data_discrete.shape)
     return np.asarray(list(data_discrete), dtype=np.int)
 
 train_dict = {}
@@ -25,6 +27,8 @@ train_dict["folder_Y"] = "./data_dir/norm_CT/regular/"
 X_list = sorted(glob.glob(train_dict["folder_X"]+"*.nii.gz"))
 Y_list = sorted(glob.glob(train_dict["folder_Y"]+"*.nii.gz"))
 
+n_bin = 128
+
 for cnt_file, file_path in enumerate(X_list):
      
     file_name = os.path.basename(file_path)
@@ -33,22 +37,37 @@ for cnt_file, file_path in enumerate(X_list):
     print("--->",cube_x_path,"<---")
     cube_x_data = nib.load(cube_x_path).get_fdata()
     cube_y_data = nib.load(cube_y_path).get_fdata()
-    print(cube_x_data.shape)
     len_x, len_y, len_z = cube_x_data.shape
-
-    X_discrete = bin_CT(cube_x_data, n_bins=1024)
-    Y_discrete = bin_CT(cube_y_data, n_bins=1024)
-
-    XY_discrete = np.asarray([X_discrete, Y_discrete])
-    print(XY_discrete.shape)
+    pixel_corr = np.zeros((n_bin, n_bin))
     
-    pixel_corr = np.zeros((1024, 1024))
+    for iz in range(len_z):
+    
+        X_discrete = bin_CT(cube_x_data[:, :, iz], n_bins=n_bin-1)
+        Y_discrete = bin_CT(cube_y_data[:, :, iz], n_bins=n_bin-1)
+        
+        for ix in range(n_bin*n_bin):
+            pixel_corr[X_discrete[ix], Y_discrete[ix]] += 1
 
-    for ix in range(len_x):
-        for iy in range(len_y):
-            for iz in range(len_z):
-                pixel_corr[X_discrete[ix, iy, iz], Y_discrete[ix, iy, iz]] += 1
+    
+    np.save(train_dict["save_folder"]+file_name[:-7]+"_pix_cor.npy", pixel_corr)
+    
+    plt.figure(figsize=(12, 12), dpi=1200)
+    g = sns.relplot(
+        data=corr_mat,
+        x="X", y="Y", hue="counts",
+        palette="vlag", hue_norm=(-1, 1), edgecolor=".7",
+        height=10, sizes=(50, 250), size_norm=(-.2, .8),
+    )
 
-    np.save(train_dict["save_folder"]+file_name+"_pix_cor.npy")
-
+    # Tweak the figure to finalize
+    g.set(xlabel="MR", ylabel="CT", aspect="equal")
+    g.despine(left=True, bottom=True)
+    g.ax.margins(.02)
+    for label in g.ax.get_xticklabels():
+        label.set_rotation(90)
+    for artist in g.legend.legendHandles:
+        artist.set_edgecolor(".7")
+        
+    plt.savefig(train_dict["save_folder"]+file_name[:-7]+"_pix_cor.png")
+    plt.close()
 
