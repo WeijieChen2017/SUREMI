e651d6a64ae15c13b4dd0d5a09e70c6030ccd5b8
WeijieChen2017 <weijiechen1994@gmail.com>
2022-02-14 00:35:58 -0600

    Good morning!

--- model/__init__.py
+++ model/__init__.py
@@ -4,3 +4,6 @@ from .unet_models import UNet_bridge
 from .unet_models import UNet_bridge_skip
 from .unet_models import UNet_intra_skip
 from .unet_models import UNet_FC
+
+from .suremi_models import suremi
+
