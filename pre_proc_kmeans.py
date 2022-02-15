99bedd87f364c8c6e1dedca7838ed9a877fce3cc
WeijieChen2017 <weijiechen1994@gmail.com>
2022-02-14 18:36:22 -0600

    Good morning!

--- pre_proc_kmeans.py
+++ pre_proc_kmeans.py
@@ -35,7 +35,7 @@ def generate_dist_weights(data_shape):
 
 def dist_kmeans(X_path, nX_clusters, dist):
     X_file = nib.load(X_path)
-    X_data = bin_CT(X_file.get_fdata(), n_bin=n_bin)
+    X_data = bin_CT(X_file.get_fdata(), n_bin=256)
     
     X_cluster = cluster.KMeans(n_clusters=nX_clusters)
     X_flatten = np.ravel(X_data)
@@ -75,7 +75,6 @@ X_list = sorted(glob.glob(train_dict["folder_X"]+"*.nii.gz"))
 Y_list = sorted(glob.glob(train_dict["folder_Y"]+"*.nii.gz"))
 
 nX_clusters = 10
-n_bin = 256
 dist = generate_dist_weights(nib.load(X_list[0]).get_fdata().shape)
 
 for cnt_file, file_path in enumerate(X_list):
