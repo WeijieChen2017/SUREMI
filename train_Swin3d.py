9c8f79332f2f6c2d8e582ac59db5d905a04e14b1
WeijieChen2017 <weijiechen1994@gmail.com>
2022-02-16 19:17:23 -0600

    Good morning!

--- train_Swin3d.py
+++ train_Swin3d.py
@@ -220,8 +220,12 @@ for idx_epoch in range(train_dict["epochs"]):
                 print("Loss: ", epoch_loss[idx_bloss])
                 idx_bloss += 1
 
-                batch_x = np.zeros((train_dict["batch"], 3, x_data.shape[2]//3, x_data.shape[0], x_data.shape[1]))
-                batch_y = np.zeros((train_dict["batch"], 3, y_data.shape[2]//3, y_data.shape[0], y_data.shape[1]))
+                del batch_x, batch_y
+                gc.collect()
+                torch.cuda.empty_cache()
+
+                batch_x = np.zeros((train_dict["batch"], 3, train_dict["channel"], x_data.shape[0], x_data.shape[1]))
+                batch_y = np.zeros((train_dict["batch"], 3, train_dict["channel"], y_data.shape[0], y_data.shape[1]))
 
         print("===>===> Epoch[{:03d}]: ".format(idx_epoch+1), end='')
         print("Loss: ", np.mean(epoch_loss))
