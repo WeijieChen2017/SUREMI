463228a3b40e82fcaea6a511f754d0825a7243ae
WeijieChen2017 <weijiechen1994@gmail.com>
2022-02-16 21:55:14 -0600

    Good morning!

--- pre_proc.py
+++ pre_proc.py
@@ -62,7 +62,7 @@ pre_proc_dict["name_orig"] = "*.nii.gz"
 pre_proc_dict["dir_syn"] = "./data_dir/Iman_CT/"
 pre_proc_dict["is_seg"] = False
 pre_proc_dict["attr_seg"] = ["norm"]
-pre_proc_dict["range_seg"] = [[0, 3000]]
+pre_proc_dict["range_seg"] = [[0, 4000]]
 pre_proc_dict["note"] = []
 pre_proc_dict["time_stamp"] = time.strftime("%Y-%m-%d_%H:%M:%S", time.localtime())
 
