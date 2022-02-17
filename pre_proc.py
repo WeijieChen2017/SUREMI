76bf1cc32f2cfc679615bfd6a0db95755f894893
WeijieChen2017 <weijiechen1994@gmail.com>
2022-02-16 22:19:55 -0600

    Good morning!

--- pre_proc.py
+++ pre_proc.py
@@ -72,10 +72,10 @@ for file_path  in file_list:
     print("-"*60)
     print(file_path)
     file_nifty = nib.load(file_path)
-    file_data = file_nifty.get_fdata()
-    scl_slope = file_nifty.dataobj.slope
-    scl_inter = file_nifty.dataobj.inter
-    file_data = file_data * scl_slope + scl_inter
+    file_data = file_nifty.get_data()
+    # scl_slope = file_nifty.dataobj.slope
+    # scl_inter = file_nifty.dataobj.inter
+    # file_data = file_data * scl_slope + scl_inter
     print(np.amin(file_data), np.amax(file_data))
 
     for idx, value_range in enumerate(pre_proc_dict["range_seg"]):
