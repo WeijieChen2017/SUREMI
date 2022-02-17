b8eaf4e549f9d9c209e4efcb100be96181b750a9
WeijieChen2017 <weijiechen1994@gmail.com>
2022-02-16 21:48:08 -0600

    Good morning!

--- train_VRT.py
+++ train_VRT.py
@@ -227,10 +227,10 @@ for idx_epoch in range(train_dict["epochs"]):
             np.save(train_dict["save_folder"]+"npy/Epoch[{:03d}]_Case[{}]_"+iter_tag+"_y.npy".format(idx_epoch+1, file_name), batch_y.cpu().detach().numpy())
             np.save(train_dict["save_folder"]+"npy/Epoch[{:03d}]_Case[{}]_"+iter_tag+"_z.npy".format(idx_epoch+1, file_name), y_hat.cpu().detach().numpy())
 
-            if np.mean(epoch_loss) < best_val_loss:
+            if np.mean(case_loss) < best_val_loss:
                 # save the best model
                 torch.save(model, train_dict["save_folder"]+"model_best_{:03d}.pth".format(idx_epoch+1))
                 print("Checkpoint saved at Epoch {:03d}".format(idx_epoch+1))
-                best_val_loss = np.mean(epoch_loss)
+                best_val_loss = np.mean(case_loss)
 
         torch.cuda.empty_cache()
