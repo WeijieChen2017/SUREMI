a32f327135e4f56c13e454f631d91e02e4b56305
WeijieChen2017 <weijiechen1994@gmail.com>
2022-02-17 14:54:11 -0600

    Good morning!

--- train_VRT.py
+++ train_VRT.py
@@ -30,7 +30,7 @@ train_dict["batch"] = 1
 train_dict["dropout"] = 0
 train_dict["model_term"] = "VRT"
 train_dict["deconv_channels"] = 6
-train_dict["input_size"] = [6,96,96]
+train_dict["input_size"] = [6,80,80]
 
 train_dict["folder_X"] = "./data_dir/Iman_MR/norm/"
 train_dict["folder_Y"] = "./data_dir/Iman_CT/norm/"
