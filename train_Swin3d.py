f23e2a12d4b09f697516e04b3ed6dc07fce08e10
WeijieChen2017 <weijiechen1994@gmail.com>
2022-02-23 21:27:01 -0600

    Good morning!

--- train_Swin3d.py
+++ train_Swin3d.py
@@ -178,7 +178,7 @@ for idx_epoch in range(train_dict["epochs"]):
         
         case_loss = np.zeros((len(file_list)))
 
-        # N, 
+        # N, C, D, H, W
         x_data = nib.load(file_list[0]).get_fdata()
 
         for cnt_file, file_path in enumerate(file_list):
@@ -196,65 +196,52 @@ for idx_epoch in range(train_dict["epochs"]):
 
             for idx_batch in range(train_dict["batch"]):
 
-                batch_x = np.zeros((train_dict["batch"], train_dict["input_size"][0], 3, train_dict["input_size"][1], train_dict["input_size"][2]))
-                batch_y = np.zeros((train_dict["batch"], train_dict["input_size"][0], 3, train_dict["input_size"][1], train_dict["input_size"][2]))
+                batch_x = np.zeros((train_dict["batch"], 1, train_dict["input_size"][0], train_dict["input_size"][1], train_dict["input_size"][2]))
+                batch_y = np.zeros((train_dict["batch"], 1, train_dict["input_size"][0], train_dict["input_size"][1], train_dict["input_size"][2]))
 
-                z_offset = np.random.randint(x_data.shape[2]//3 - train_dict["input_size"][0])
-                h_offset = np.random.randint(x_data.shape[0] - train_dict["input_size"][1])
-                w_offset = np.random.randint(x_data.shape[1] - train_dict["input_size"][2])
+                d0_offset = np.random.randint(x_data.shape[0] - train_dict["input_size"][1])
+                d1_offset = np.random.randint(x_data.shape[1] - train_dict["input_size"][2])
+                d2_offset = np.random.randint(x_data.shape[2] - train_dict["input_size"][0])
 
-                for idx_channel in range(train_dict["input_size"][0]):
-                    z_center = (z_offset + idx_channel) * 3 + 1
-                    x_slice = x_data[h_offset:h_offset+train_dict["input_size"][1], w_offset:w_offset+train_dict["input_size"][2], z_center-1:z_center+2]
-                    y_slice = y_data[h_offset:h_offset+train_dict["input_size"][1], w_offset:w_offset+train_dict["input_size"][2], z_center-1:z_center+2]
-                    
-                    # print("H:", h_offset, h_offset+train_dict["input_size"][1])
-                    # print("W:", w_offset, w_offset+train_dict["input_size"][2])
-                    # print("Z:", z_center-1, z_center+2)
-
-                    batch_x[idx_batch, idx_channel, 0, :, :] = x_slice[:, :, 0]
-                    batch_x[idx_batch, idx_channel, 1, :, :] = x_slice[:, :, 1]
-                    batch_x[idx_batch, idx_channel, 2, :, :] = x_slice[:, :, 2]
-                    batch_y[idx_batch, idx_channel, 0, :, :] = y_slice[:, :, 0]
-                    batch_y[idx_batch, idx_channel, 1, :, :] = y_slice[:, :, 1]
-                    batch_y[idx_batch, idx_channel, 2, :, :] = y_slice[:, :, 2]
+                x_slice = x_data[d0_offset:d0_offset+train_dict["input_size"][0],
+                                 d1_offset:d1_offset+train_dict["input_size"][1],
+                                 d2_offset:d2_offset+train_dict["input_size"][2]
+                                 ]
+                y_slice = y_data[d0_offset:d0_offset+train_dict["input_size"][0],
+                                 d1_offset:d1_offset+train_dict["input_size"][1],
+                                 d2_offset:d2_offset+train_dict["input_size"][2]
+                                 ]
+                batch_x[idx_batch, 0, :, :, :] = x_slice
+                batch_y[idx_batch, 0, :, :, :] = y_slice
 
             batch_x = torch.from_numpy(batch_x).float().to(device)
             batch_y = torch.from_numpy(batch_y).float().to(device)
-                print("batch_x:", batch_x.size())
-                print("batch_y:", batch_y.size())
+            print("batch_x:", batch_x.size())
+            print("batch_y:", batch_y.size())
                 
-                optimizer.zero_grad()
-                y_hat = model(batch_x)
-                # print("Yhat size: ", y_hat.size())
-                loss = criterion(y_hat, batch_y)
-                if isTrain:
-                    loss.backward()
-                    optimizer.step()
-                epoch_loss[idx_bloss] = loss.item()
-                # print("===> Epoch[{:03d}]-Batch[{:03d}]: ".format(idx_epoch+1, idx_bloss+1), end='')
-                print("Loss: ", epoch_loss[idx_bloss])
-                idx_bloss += 1
-
-                # del batch_x, batch_y
-                # gc.collect()
-                # torch.cuda.empty_cache()
-            else:
-                idx_batch += 1
+            optimizer.zero_grad()
+            y_hat = model(batch_x)
+            # print("Yhat size: ", y_hat.size())
+            loss = criterion(y_hat, batch_y)
+            if isTrain:
+                loss.backward()
+                optimizer.step()
+            case_loss[cnt_file] = loss.item()
+            print("Loss: ", case_loss[cnt_file])
 
         print(iter_tag + " ===>===> Epoch[{:03d}]: ".format(idx_epoch+1), end='')
-        print("  Loss: ", np.mean(epoch_loss))
-        np.save(train_dict["save_folder"]+"loss/epoch_loss_"+iter_tag+"_{:03d}.npy".format(idx_epoch+1), epoch_loss)
+        print("  Loss: ", np.mean(case_loss))
+        np.save(train_dict["save_folder"]+"loss/epoch_loss_"+iter_tag+"_{:03d}.npy".format(idx_epoch+1), case_loss)
 
         if isVal:
             np.save(train_dict["save_folder"]+"npy/Epoch[{:03d}]_Case[{}]_".format(idx_epoch+1, file_name)+iter_tag+"_x.npy", batch_x.cpu().detach().numpy())
             np.save(train_dict["save_folder"]+"npy/Epoch[{:03d}]_Case[{}]_".format(idx_epoch+1, file_name)+iter_tag+"_y.npy", batch_y.cpu().detach().numpy())
             np.save(train_dict["save_folder"]+"npy/Epoch[{:03d}]_Case[{}]_".format(idx_epoch+1, file_name)+iter_tag+"_z.npy", y_hat.cpu().detach().numpy())
 
-            if np.mean(epoch_loss) < best_val_loss:
+            if np.mean(case_loss) < best_val_loss:
                 # save the best model
                 torch.save(model, train_dict["save_folder"]+"model_best_{:03d}.pth".format(idx_epoch+1))
                 print("Checkpoint saved at Epoch {:03d}".format(idx_epoch+1))
-                best_val_loss = np.mean(epoch_loss)
+                best_val_loss = np.mean(case_loss)
 
         torch.cuda.empty_cache()
