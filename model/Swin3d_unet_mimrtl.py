6fe59412cf80fc4f01fdaa34f9ac28ca2dac964e
WeijieChen2017 <weijiechen1994@gmail.com>
2022-02-26 22:34:38 -0600

    Good morning!

--- model/Swin3d_unet_mimrtl.py
+++ model/Swin3d_unet_mimrtl.py
@@ -366,7 +366,7 @@ def compute_mask(D, H, W, window_size, shift_size, device):
     return attn_mask
 
 
-class BasicLayer(nn.Module):
+class BasicLayer_down(nn.Module):
     """ A basic Swin Transformer layer for one stage.
     Args:
         dim (int): Number of feature channels
@@ -444,6 +444,88 @@ class BasicLayer(nn.Module):
 
         if self.downsample is not None:
             x = self.downsample(x)
+        x = rearrange(x, 'b d h w c -> b c d h w')
+        return x
+
+
+class BasicLayer_up(nn.Module):
+    """ A basic Swin Transformer layer for one stage.
+    Args:
+        dim (int): Number of feature channels
+        depth (int): Depths of this stage.
+        num_heads (int): Number of attention head.
+        window_size (tuple[int]): Local window size. Default: (1,7,7).
+        mlp_ratio (float): Ratio of mlp hidden dim to embedding dim. Default: 4.
+        qkv_bias (bool, optional): If True, add a learnable bias to query, key, value. Default: True
+        qk_scale (float | None, optional): Override default qk scale of head_dim ** -0.5 if set.
+        drop (float, optional): Dropout rate. Default: 0.0
+        attn_drop (float, optional): Attention dropout rate. Default: 0.0
+        drop_path (float | tuple[float], optional): Stochastic depth rate. Default: 0.0
+        norm_layer (nn.Module, optional): Normalization layer. Default: nn.LayerNorm
+        downsample (nn.Module | None, optional): Downsample layer at the end of the layer. Default: None
+    """
+
+    def __init__(self,
+                 dim,
+                 depth,
+                 num_heads,
+                 window_size=(7,7,7),
+                 mlp_ratio=4.,
+                 qkv_bias=False,
+                 qk_scale=None,
+                 drop=0.,
+                 attn_drop=0.,
+                 drop_path=0.,
+                 norm_layer=nn.LayerNorm,
+                 upsample=None,
+                 use_checkpoint=False):
+        super().__init__()
+        self.window_size = window_size
+        self.shift_size = tuple(i // 2 for i in window_size)
+        self.depth = depth
+        self.use_checkpoint = use_checkpoint
+
+        # build blocks
+        self.blocks = nn.ModuleList([
+            SwinTransformerBlock3D(
+                dim=dim,
+                num_heads=num_heads,
+                window_size=window_size,
+                shift_size=(0,0,0) if (i % 2 == 0) else self.shift_size,
+                mlp_ratio=mlp_ratio,
+                qkv_bias=qkv_bias,
+                qk_scale=qk_scale,
+                drop=drop,
+                attn_drop=attn_drop,
+                drop_path=drop_path[i] if isinstance(drop_path, list) else drop_path,
+                norm_layer=norm_layer,
+                use_checkpoint=use_checkpoint,
+            )
+            for i in range(depth)])
+        
+        self.upsample = upsample
+        if self.upsample is not None:
+            self.upsample = upsample(dim=dim, norm_layer=norm_layer)
+
+    def forward(self, x):
+        """ Forward function.
+        Args:
+            x: Input feature, tensor size (B, C, D, H, W).
+        """
+        # calculate attention mask for SW-MSA
+        B, C, D, H, W = x.shape
+        window_size, shift_size = get_window_size((D,H,W), self.window_size, self.shift_size)
+        x = rearrange(x, 'b c d h w -> b d h w c')
+        Dp = int(np.ceil(D / window_size[0])) * window_size[0]
+        Hp = int(np.ceil(H / window_size[1])) * window_size[1]
+        Wp = int(np.ceil(W / window_size[2])) * window_size[2]
+        attn_mask = compute_mask(Dp, Hp, Wp, window_size, shift_size, x.device)
+        for blk in self.blocks:
+            x = blk(x, attn_mask)
+        x = x.view(B, D, H, W, -1)
+
+        if self.upsample is not None:
+            x = self.upsample(x)
         x = rearrange(x, 'b d h w c -> b c d h w')
         return x
 
@@ -558,7 +640,7 @@ class SwinTransformer3D(nn.Module):
         # build layers
         self.encoder_layers = nn.ModuleList()
         for i_layer in range(self.num_layers):
-            layer = BasicLayer(
+            layer = BasicLayer_down(
                 dim=int(embed_dim * 2**i_layer),
                 depth=depths[i_layer],
                 num_heads=num_heads[i_layer],
@@ -582,24 +664,24 @@ class SwinTransformer3D(nn.Module):
         self._freeze_stages()
 
 
-        # self.decode_layers = nn.ModuleList()
-        # for i_layer in range(self.num_layers):
-        #     layer = BasicLayer_deconv(
-        #         dim=int(embed_dim * 2**i_layer),
-        #         depth=depths[i_layer],
-        #         num_heads=num_heads[i_layer],
-        #         window_size=window_size,
-        #         mlp_ratio=mlp_ratio,
-        #         qkv_bias=qkv_bias,
-        #         qk_scale=qk_scale,
-        #         drop=drop_rate,
-        #         attn_drop=attn_drop_rate,
-        #         drop_path=dpr[sum(depths[:i_layer]):sum(depths[:i_layer + 1])],
-        #         norm_layer=norm_layer,
-        #         downsample=PatchMerging if i_layer<self.num_layers-1 else None,
-        #         use_checkpoint=use_checkpoint)
-        #     self.decode_layers.append(layer)
-        # self.decode_layers.reverse()
+        self.decode_layers = nn.ModuleList()
+        for i_layer in range(self.num_layers):
+            layer = BasicLayer_up(
+                dim=int(embed_dim * 2**i_layer),
+                depth=depths[i_layer],
+                num_heads=num_heads[i_layer],
+                window_size=window_size,
+                mlp_ratio=mlp_ratio,
+                qkv_bias=qkv_bias,
+                qk_scale=qk_scale,
+                drop=drop_rate,
+                attn_drop=attn_drop_rate,
+                drop_path=dpr[sum(depths[:i_layer]):sum(depths[:i_layer + 1])],
+                norm_layer=norm_layer,
+                downsample=PatchUnMerging if i_layer<self.num_layers-1 else None,
+                use_checkpoint=use_checkpoint)
+            self.decode_layers.append(layer)
+        self.decode_layers.reverse()
 
 
     def _freeze_stages(self):
@@ -732,6 +814,11 @@ class SwinTransformer3D(nn.Module):
         x_list.append(x)
         # torch.Size([1, 1024, 9, 8, 8])
         
+        for layer in self.decoder_layers:
+            print("Decoder: ", x.size())
+            x = layer(x.contiguous())
+            x_list.append(x)
+
         for data in x_list:
             print(data.size())
         print("End")
