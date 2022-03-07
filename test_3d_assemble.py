6acacd963edd72b4f14352feabb709b3b6487c46
WeijieChen2017 <weijiechen1994@gmail.com>
2022-03-06 19:25:55 -0600

    Good morning!

--- test_3d_assemble.py
+++ test_3d_assemble.py
@@ -139,7 +139,7 @@ for cnt_file, file_path in enumerate(file_list):
                         for iiz in range(train_dict["input_size"][2]):
                             print("-"*60)
                             curr_idx = cnt_cube_y_hat[sx+iix, sy+iiy, sz+iiz]
-                            print(curr_idx, sx+iix, sy+iiy, sz+iiz)
+                            print(curr_idx)
                             # print(iix, iiy, iiz)
                             pad_y_hat[curr_idx, sx+iix, sy+iiy, sz+iiz] = detach_batch_z[iix, iiy, iiz]
                             cnt_cube_y_hat[sx+iix, sy+iiy, sz+iiz] += 1
