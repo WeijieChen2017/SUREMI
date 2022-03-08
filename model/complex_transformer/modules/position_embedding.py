8b44730899b03cedbd1ba9054b9966ec3c9a6633
WeijieChen2017 <weijiechen1994@gmail.com>
2022-03-07 19:57:25 -0600

    Good morning!

--- model/complex_transformer/modules/position_embedding.py
+++ model/complex_transformer/modules/position_embedding.py
@@ -1,8 +1,7 @@
 import math
-
 import torch
 import torch.nn as nn
-
+from torch.nn.parameter import Parameter
 
 def make_positions(tensor, padding_idx, left_pad):
     """Replace non-padding symbols with their position numbers.
