57b30c0c20558d7b9aed26f9797ed30be69cc33f
WeijieChen2017 <weijiechen1994@gmail.com>
2022-03-12 15:00:43 -0600

    Good morning!

--- train_CTGM_2d.py
+++ train_CTGM_2d.py
@@ -143,8 +143,8 @@ best_val_loss = 1
 best_epoch = 0
 # wandb.watch(model)
 
-# package_train = [train_list[:10], True, False, "train"]
-package_train = [train_list, True, True, "train"]
+package_train = [train_list[:10], True, False, "train"]
+# package_train = [train_list, True, True, "train"]
 package_val = [val_list, False, True, "val"]
 # package_test = [test_list, False, False, "test"]
 
