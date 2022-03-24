a70dcc5823be91e42f6a558ebee4129553babe6f
WeijieChen2017 <weijiechen1994@gmail.com>
2022-03-24 16:51:07 -0500

    Good morning!

--- train_CTGM_2d_mse_ddp.py
+++ train_CTGM_2d_mse_ddp.py
@@ -16,17 +16,28 @@ import requests
 from model import ComplexTransformerGenerationModel as CTGM
 from torch.nn.parallel import DistributedDataParallel as DDP
 import torch.distributed as dist
+
+
+# ==================== DDP setting ====================
+
+os.environ['MASTER_ADDR'] = 'localhost'
+os.environ['MASTER_PORT'] = '12355'
+
+# initialize the process group
+dist.init_process_group("nccl", rank=rank, world_size=world_size)
+
+
 # ==================== dict and config ====================
 
 train_dict = {}
 train_dict["time_stamp"] = time.strftime("%Y-%m-%d_%H:%M:%S", time.localtime())
-train_dict["project_name"] = "CTGM_2d_v2_mse"
+train_dict["project_name"] = "CTGM_2d_v2_mse_102_ddp"
 train_dict["save_folder"] = "./project_dir/"+train_dict["project_name"]+"/"
 train_dict["seed"] = 426
 train_dict["input_size"] = [256, 256]
 ax, ay = train_dict["input_size"]
 # train_dict["gpu_ids"] = [1,2,4,6]
-train_dict["epochs"] = 500
+train_dict["epochs"] = 600
 train_dict["batch"] = 16 * 4
 train_dict["dropout"] = 0
 train_dict["model_term"] = "ComplexTransformerGenerationModel"
@@ -65,51 +76,8 @@ for path in [train_dict["save_folder"], train_dict["save_folder"]+"npy/", train_
         os.mkdir(path)
 
 np.save(train_dict["save_folder"]+"dict.npy", train_dict)
-
-
-# ==================== basic settings ====================
-
 np.random.seed(train_dict["seed"])
-# gpu_list = ','.join(str(x) for x in train_dict["gpu_ids"])
-# os.environ['CUDA_VISIBLE_DEVICES'] = gpu_list
-# print('export CUDA_VISIBLE_DEVICES=' + gpu_list)
-
-local_rank = int(os.environ["LOCAL_RANK"])
-torch.cuda.set_device(local_rank)
-dist.init_process_group(backend='nccl')
-device = torch.device("cuda", local_rank, world_size=4)
-print("Local rank:", local_rank)
-
-# model = CTGM( 
-#     input_dims=train_dict["model_related"]["input_dims"],
-#     hidden_size=train_dict["model_related"]["hidden_size"],
-#     embed_dim=train_dict["model_related"]["embed_dim"],
-#     output_dim=train_dict["model_related"]["output_dim"],
-#     num_heads=train_dict["model_related"]["num_heads"],
-#     attn_dropout=train_dict["model_related"]["attn_dropout"],
-#     relu_dropout=train_dict["model_related"]["relu_dropout"],
-#     res_dropout=train_dict["model_related"]["res_dropout"],
-#     out_dropout=train_dict["model_related"]["out_dropout"],
-#     layers=train_dict["model_related"]["layers"],
-#     attn_mask=train_dict["model_related"]["attn_mask"])
-
-model = torch.load(train_dict["save_folder"]+"model_best_102.pth")
-model = DDP(model, device_ids=[local_rank], output_device=local_rank)
-print("The model has been set at", local_rank)
-
-# model = nn.DataParallel(model)
-model.train()
-model = model.to(device)
-criterion = nn.MSELoss()
-
-optimizer = torch.optim.AdamW(
-    model.parameters(),
-    lr = train_dict["opt_lr"],
-    betas = train_dict["opt_betas"],
-    eps = train_dict["opt_eps"],
-    weight_decay = train_dict["opt_weight_decay"],
-    amsgrad = train_dict["amsgrad"]
-    )
+best_val_loss = 3
 
 # ==================== data division ====================
 
@@ -133,54 +101,104 @@ data_division_dict = {
     "test_list_X" : test_list}
 np.save(train_dict["save_folder"]+"data_division.npy", data_division_dict)
 
-# ==================== training ====================
 
-best_val_loss = 3
-best_epoch = 0
-world_size = 4
-# wandb.watch(model)
+# ==================== basic settings ====================
 
-# package_train = [train_list[:1], True, False, "train"]
-package_train = [train_list, True, True, "train"]
-package_val = [val_list, False, True, "val"]
-# package_test = [test_list, False, False, "test"]
+# gpu_list = ','.join(str(x) for x in train_dict["gpu_ids"])
+# os.environ['CUDA_VISIBLE_DEVICES'] = gpu_list
+# print('export CUDA_VISIBLE_DEVICES=' + gpu_list)
 
-num_vocab = (ax//cx) * (ay//cx)
+# local_rank = int(os.environ["LOCAL_RANK"])
+# torch.cuda.set_device(local_rank)
+# dist.init_process_group(backend='nccl')
+# device = torch.device("cuda", local_rank, world_size=4)
+# print("Local rank:", local_rank)
 
-for idx_epoch_new in range(train_dict["epochs"]):
-    idx_epoch = idx_epoch_new + 108
-    print("~~~~~~Epoch[{:03d}]~~~~~~".format(idx_epoch+1))
+# model = CTGM( 
+#     input_dims=train_dict["model_related"]["input_dims"],
+#     hidden_size=train_dict["model_related"]["hidden_size"],
+#     embed_dim=train_dict["model_related"]["embed_dim"],
+#     output_dim=train_dict["model_related"]["output_dim"],
+#     num_heads=train_dict["model_related"]["num_heads"],
+#     attn_dropout=train_dict["model_related"]["attn_dropout"],
+#     relu_dropout=train_dict["model_related"]["relu_dropout"],
+#     res_dropout=train_dict["model_related"]["res_dropout"],
+#     out_dropout=train_dict["model_related"]["out_dropout"],
+#     layers=train_dict["model_related"]["layers"],
+#     attn_mask=train_dict["model_related"]["attn_mask"])
 
-    for package in [package_train, package_val]: # , package_val
+def run_demo(demo_fn, world_size):
+    mp.spawn(demo_fn,
+             args=(world_size,),
+             nprocs=world_size,
+             join=True)
 
-        file_list = package[0]
-        isTrain = package[1]
-        isVal = package[2]
-        iter_tag = package[3]
 
-        if isTrain:
-            model.train()
-        else:
-            model.eval()
+def cleanup():
+    dist.destroy_process_group()
 
-        random.shuffle(file_list)
-        
-        case_loss = np.zeros((len(file_list)))
 
-        """
-        x should have dimension [seq_len, batch_size, n_features] (i.e., L, N, C).
-        """
+def demo_basic(rank, world_size):
+    print(f"Running basic DDP example on rank {rank}.")
+    setup(rank, world_size)
 
-        for cnt_file, file_path in enumerate(file_list):
+    # create model and move it to GPU with id rank
+
+    model = torch.load("./project_dir/CTGM_2d_v2_mse_102_ddp/model_best_102.pth").to(rank)
+    ddp_model = DDP(model, device_ids=[rank])
+    print("The model has been set at", rank)
+    model.train()
+    criterion = nn.MSELoss()
+
+    optimizer = torch.optim.AdamW(
+        ddp_model.parameters(),
+        lr = train_dict["opt_lr"],
+        betas = train_dict["opt_betas"],
+        eps = train_dict["opt_eps"],
+        weight_decay = train_dict["opt_weight_decay"],
+        amsgrad = train_dict["amsgrad"]
+        )
+
+
+    # package_train = [train_list[:1], True, False, "train"]
+    package_train = [train_list, True, True, "train"]
+    package_val = [val_list, False, True, "val"]
+    # package_test = [test_list, False, False, "test"]
+
+
+    for idx_epoch_new in range(train_dict["epochs"]):
+        idx_epoch = idx_epoch_new + 102
+        print("~Epoch[{:03d}]~".format(idx_epoch+1), end="")
+
+        for package in [package_train, package_val]: # , package_val
+
+            file_list = package[0]
+            isTrain = package[1]
+            isVal = package[2]
+            iter_tag = package[3]
+
+            if isTrain:
+                model.train()
+            else:
+                model.eval()
+
+            random.shuffle(file_list)
             
+            case_loss = np.zeros((len(file_list)))
             total_file = len(file_list)
+            total_group = len(file_list)//world_size
+            """
+            x should have dimension [seq_len, batch_size, n_features] (i.e., L, N, C).
+            """
 
-            if cnt_file % 4 == local_rank:
-            
+            for idx_file_group in range(len(file_list)//world_size):
+
+                print(iter_tag + " [{:03d}]/[{:03d}]:".format(idx_file_group+1, total_group), end=" ") #
+                
+                file_path = idx_file_group * train_dict["cnt_gpu"] + rank
                 x_path = file_path
                 y_path = file_path.replace("MR", "CT")
                 file_name = os.path.basename(file_path)
-                print(iter_tag + " ===> Epoch[{:03d}]-[{:03d}]/[{:03d}]: --->".format(idx_epoch+1, cnt_file+1, total_file), file_name, "<---", end="") #
                 x_data = np.load(x_path)
                 y_data = np.load(y_path)
                 dz = x_data.shape[0]
@@ -201,9 +219,9 @@ for idx_epoch_new in range(train_dict["epochs"]):
                         batch_x[:, iz, :] = x_data[z_list[iz+batch_offset], :, :]
                         batch_y[:, iz, :] = y_data[z_list[iz+batch_offset], :, :]
 
-                    batch_x = torch.from_numpy(batch_x).float().to(device).contiguous()
-                    batch_y = torch.from_numpy(batch_y).float().to(device).contiguous()
-                        
+                    batch_x = torch.from_numpy(batch_x).float().to(rank).contiguous()
+                    batch_y = torch.from_numpy(batch_y).float().to(rank).contiguous()
+
                     optimizer.zero_grad()
                     # print(batch_x.size(), batch_y.size())
                     y_hat = model(batch_x, batch_y)
@@ -219,16 +237,25 @@ for idx_epoch_new in range(train_dict["epochs"]):
                     loss /= world_size
                     batch_loss[ib] = loss.item()
 
-                case_loss[cnt_file] = np.mean(batch_loss)
-                print("Loss: ", case_loss[cnt_file])
+            print("-> Loss: ", np.mean(case_loss))
+            np.save(train_dict["save_folder"]+"loss/epoch_loss_"+iter_tag+"_{:03d}_rank{:01d}.npy".format(idx_epoch+1, rank), case_loss)
 
-        print(iter_tag + " ===>===> Epoch[{:03d}]: ".format(idx_epoch+1), end='')
-        print("  Loss: ", np.mean(case_loss))
-        np.save(train_dict["save_folder"]+"loss/epoch_loss_"+iter_tag+"_{:03d}.npy".format(idx_epoch+1), case_loss)
+            if isVal:
+                if np.mean(case_loss) < best_val_loss:
+                    # save the best model
+                    torch.save(model, train_dict["save_folder"]+"model_best_ddp_{:03d}_rank{}.pth".format(idx_epoch+1, rank))
+                    print("Checkpoint saved at Epoch {:03d}".format(idx_epoch + 1))
+                    best_val_loss = np.mean(case_loss)
 
-        if isVal:
-            if np.mean(case_loss) < best_val_loss:
-                # save the best model
-                torch.save(model, train_dict["save_folder"]+"model_best_ddp_{:03d}.pth".format(idx_epoch + 1))
-                print("Checkpoint saved at Epoch {:03d}".format(idx_epoch + 1))
-                best_val_loss = np.mean(case_loss)
+    cleanup()
+
+
+gpu_list = ','.join(str(x) for x in [1,3,4,6])
+os.environ['CUDA_VISIBLE_DEVICES'] = gpu_list
+print('export CUDA_VISIBLE_DEVICES=' + gpu_list)
+n_gpus = torch.cuda.device_count()
+assert n_gpus >= 2, f"Requires at least 2 GPUs to run, but got {n_gpus}"
+world_size = n_gpus
+run_demo(demo_basic, world_size)
+
+
