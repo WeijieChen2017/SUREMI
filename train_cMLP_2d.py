d928d7ff04ca26c091ed1854fc25db71cd11daac
WeijieChen2017 <weijiechen1994@gmail.com>
2022-03-31 14:58:25 -0500

    Good morning!

---
+++ train_cMLP_2d.py
@@ -0,0 +1,242 @@
+import os
+import gc
+import glob
+import time
+# import wandb
+import random
+
+import numpy as np
+import nibabel as nib
+import torch.nn as nn
+
+import torch
+import torchvision
+import requests
+
+from model import cMLP
+
+# ==================== dict and config ====================
+
+train_dict = {}
+train_dict["time_stamp"] = time.strftime("%Y-%m-%d_%H:%M:%S", time.localtime())
+train_dict["project_name"] = "CTGM_2d_v9_cMLP"
+train_dict["save_folder"] = "./project_dir/"+train_dict["project_name"]+"/"
+train_dict["seed"] = 729
+train_dict["input_size"] = [256, 256]
+ax, ay = train_dict["input_size"]
+train_dict["gpu_ids"] = [1]
+train_dict["epochs"] = 100
+train_dict["batch"] = 16
+train_dict["dropout"] = 0
+train_dict["model_term"] = "ComplexTransformerGenerationModel"
+
+train_dict["model_related"] = {}
+train_dict["model_related"]["cx"] = 32
+cx = train_dict["model_related"]["cx"]
+train_dict["model_related"]["in_dim"] = cx*cx
+train_dict["model_related"]["mid_dim_1"] = 1024
+train_dict["model_related"]["mid_dim_2"] = 1024
+train_dict["model_related"]["out_dim"] = cx*cx
+
+train_dict["folder_X"] = "./data_dir/Iman_MR/kspace_2d_norm/"
+train_dict["folder_Y"] = "./data_dir/Iman_CT/kspace_2d_norm/"
+# train_dict["pre_train"] = "swin_base_patch244_window1677_kinetics400_22k.pth"
+train_dict["val_ratio"] = 0.3
+train_dict["test_ratio"] = 0.2
+
+train_dict["loss_term"] = "MSELoss"
+train_dict["optimizer"] = "AdamW"
+train_dict["opt_lr"] = 1e-3 # default
+train_dict["opt_betas"] = (0.9, 0.999) # default
+train_dict["opt_eps"] = 1e-8 # default
+train_dict["opt_weight_decay"] = 0.01 # default
+train_dict["amsgrad"] = False # default
+
+for path in [train_dict["save_folder"], train_dict["save_folder"]+"npy/", train_dict["save_folder"]+"loss/"]:
+    if not os.path.exists(path):
+        os.mkdir(path)
+
+# wandb.init(project=train_dict["project_name"])
+# config = wandb.config
+# config.in_chan = train_dict["input_channel"]
+# config.out_chan = train_dict["output_channel"]
+# config.epochs = train_dict["epochs"]
+# config.batch = train_dict["batch"]
+# config.dropout = train_dict["dropout"]
+# config.moodel_term = train_dict["model_term"]
+# config.loss_term = train_dict["loss_term"]
+# config.opt_lr = train_dict["opt_lr"]
+# config.opt_weight_decay = train_dict["opt_weight_decay"]
+
+np.save(train_dict["save_folder"]+"dict.npy", train_dict)
+
+
+# ==================== basic settings ====================
+
+np.random.seed(train_dict["seed"])
+gpu_list = ','.join(str(x) for x in train_dict["gpu_ids"])
+os.environ['CUDA_VISIBLE_DEVICES'] = gpu_list
+print('export CUDA_VISIBLE_DEVICES=' + gpu_list)
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+model = cMLP(
+    in_dim = train_dict["model_related"]["in_dim"],
+    mid_dim_1 = train_dict["model_related"]["mid_dim_1"],
+    mid_dim_2 = train_dict["model_related"]["mid_dim_2"],
+    out_dim = train_dict["model_related"]["out_dim"])
+
+# model = torch.load(train_dict["save_folder"]+"model_best_102.pth")
+
+# model = nn.DataParallel(model)
+model.train()
+model = model.to(device)
+criterion = nn.SmoothL1Loss()
+
+optimizer = torch.optim.AdamW(
+    model.parameters(),
+    lr = train_dict["opt_lr"],
+    betas = train_dict["opt_betas"],
+    eps = train_dict["opt_eps"],
+    weight_decay = train_dict["opt_weight_decay"],
+    amsgrad = train_dict["amsgrad"]
+    )
+
+# ==================== data division ====================
+
+X_list = sorted(glob.glob(train_dict["folder_X"]+"*.npy"))
+Y_list = sorted(glob.glob(train_dict["folder_Y"]+"*.npy"))
+
+selected_list = np.asarray(X_list)
+np.random.shuffle(selected_list)
+selected_list = list(selected_list)
+
+val_list = selected_list[:int(len(selected_list)*train_dict["val_ratio"])]
+val_list.sort()
+test_list = selected_list[-int(len(selected_list)*train_dict["test_ratio"]):]
+test_list.sort()
+train_list = list(set(selected_list) - set(val_list) - set(test_list))
+train_list.sort()
+
+data_division_dict = {
+    "train_list_X" : train_list,
+    "val_list_X" : val_list,
+    "test_list_X" : test_list}
+np.save(train_dict["save_folder"]+"data_division.npy", data_division_dict)
+
+# ==================== training ====================
+
+best_val_loss = 1e-4
+best_epoch = 0
+# wandb.watch(model)
+
+# package_train = [train_list[:1], True, False, "train"]
+package_train = [train_list, True, True, "train"]
+package_val = [val_list, False, True, "val"]
+# package_test = [test_list, False, False, "test"]
+
+num_vocab = (ax//cx) * (ay//cx)
+
+for idx_epoch_new in range(train_dict["epochs"]):
+    idx_epoch = idx_epoch_new + 0
+    print("~~~~~~Epoch[{:03d}]~~~~~~".format(idx_epoch+1))
+
+    for package in [package_train, package_val]: # , package_val
+
+        file_list = package[0]
+        isTrain = package[1]
+        isVal = package[2]
+        iter_tag = package[3]
+
+        if isTrain:
+            model.train()
+        else:
+            model.eval()
+
+        random.shuffle(file_list)
+        
+        case_loss = np.zeros((len(file_list)))
+
+        """
+        batch_x [seq_len, batch_size, n_features] (i.e., L, N, C).
+        x_data [batch_size, seq_len or num_vocab, n_features/cx*cx*2]
+        """
+
+        for cnt_file, file_path in enumerate(file_list):
+            
+            total_file = len(file_list)
+            
+            x_path = file_path
+            y_path = file_path.replace("MR", "CT")
+            file_name = os.path.basename(file_path)
+            print(iter_tag + " ===> Epoch[{:03d}]-[{:03d}]/[{:03d}]: --->".format(idx_epoch+1, cnt_file+1, total_file), file_name, "<---", end="") #
+            x_data = np.load(x_path)
+            y_data = np.load(y_path)
+            dz = x_data.shape[0]
+            z_list = list(range(dz))
+            random.shuffle(z_list)
+            batch_per_step = train_dict["batch"]
+            # batch_per_step = dz
+            batch_loss = np.zeros((dz // batch_per_step))
+
+            for ib in range(dz // batch_per_step):
+
+                batch_x = np.zeros((num_vocab*batch_per_step, cx*cx))
+                batch_y = np.zeros((num_vocab*batch_per_step, cx*cx))
+                batch_offset = ib * batch_per_step
+
+                for iz in range(batch_per_step):
+
+                    x_real = x_data[z_list[iz+batch_offset], :, :cx*cx]
+                    x_imag = x_data[z_list[iz+batch_offset], :, cx*cx:]
+                    y_real = y_data[z_list[iz+batch_offset], :, :cx*cx]
+                    y_imag = y_data[z_list[iz+batch_offset], :, cx*cx:]
+
+                    x_c = np.vectorize(complex)(x_real, x_imag).reshape(num_vocab, cx*cx)
+                    y_c = np.vectorize(complex)(y_real, y_imag).reshape(num_vocab, cx*cx)
+
+                    batch_x[(iz-1)*num_vocab:iz*num_vocab, :] = x_c
+                    batch_y[(iz-1)*num_vocab:iz*num_vocab, :] = x_y
+                    
+                batch_x = torch.from_numpy(batch_x).float().to(device).contiguous()
+                batch_y = torch.from_numpy(batch_y).float().to(device).contiguous()
+                    
+                optimizer.zero_grad()
+                # print(batch_x.size(), batch_y.size())
+                y_hat = model(batch_x, batch_y)
+                # print("Yhat size: ", y_hat.size(), end="   ")
+                # print("Ytrue size: ", batch_y.size())
+                loss = criterion(y_hat, batch_y)
+                if isTrain:
+                    loss.backward()
+                    optimizer.step()
+                batch_loss[ib] = loss.item()
+
+            case_loss[cnt_file] = np.mean(batch_loss)
+            print("Loss: ", case_loss[cnt_file])
+
+            if cnt_file < len(file_list)-1:
+                del batch_x, batch_y
+                gc.collect()
+                torch.cuda.empty_cache()
+
+        print(iter_tag + " ===>===> Epoch[{:03d}]: ".format(idx_epoch+1), end='')
+        print("  Loss: ", np.mean(case_loss))
+        np.save(train_dict["save_folder"]+"loss/epoch_loss_"+iter_tag+"_{:03d}.npy".format(idx_epoch+1), case_loss)
+
+
+
+        # if idx_epoch % 10 == 1:
+        #     np.save(train_dict["save_folder"]+"npy/Epoch[{:03d}]_Case[{}]_".format(idx_epoch+1, file_name)+iter_tag+"_x.npy", batch_x.cpu().detach().numpy())
+        #     np.save(train_dict["save_folder"]+"npy/Epoch[{:03d}]_Case[{}]_".format(idx_epoch+1, file_name)+iter_tag+"_y.npy", batch_y.cpu().detach().numpy())
+        #     np.save(train_dict["save_folder"]+"npy/Epoch[{:03d}]_Case[{}]_".format(idx_epoch+1, file_name)+iter_tag+"_z.npy", y_hat.cpu().detach().numpy())
+        #     torch.save(model, train_dict["save_folder"]+"model_{:03d}.pth".format(idx_epoch + 1))
+        if isVal:
+            if np.mean(case_loss) < best_val_loss:
+                # save the best model
+                torch.save(model, train_dict["save_folder"]+"model_best_{:03d}.pth".format(idx_epoch + 1))
+                print("Checkpoint saved at Epoch {:03d}".format(idx_epoch + 1))
+                best_val_loss = np.mean(case_loss)
+
+        del batch_x, batch_y
+        gc.collect()
+        torch.cuda.empty_cache()
