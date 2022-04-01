6403ee3ff3bc6ac80f23884e2a2b841553e43e47
WeijieChen2017 <weijiechen1994@gmail.com>
2022-04-01 01:17:00 -0500

    Good morning!

--- train_cMLP_2d.py
+++ train_cMLP_2d.py
@@ -19,7 +19,7 @@ from model import cMLP
 
 train_dict = {}
 train_dict["time_stamp"] = time.strftime("%Y-%m-%d_%H:%M:%S", time.localtime())
-train_dict["project_name"] = "CTGM_2d_v10_cMLP_mse"
+train_dict["project_name"] = "CTGM_2d_v9_cMLP_SL1"
 train_dict["save_folder"] = "./project_dir/"+train_dict["project_name"]+"/"
 train_dict["seed"] = 729
 train_dict["input_size"] = [256, 256]
@@ -87,7 +87,8 @@ model = cMLP(
 # model = nn.DataParallel(model)
 model.train()
 model = model.to(device)
-criterion = nn.MSELoss()
+# criterion = nn.MSELoss()
+criterion = nn.SmoothL1Loss()
 
 optimizer = torch.optim.AdamW(
     model.parameters(),
