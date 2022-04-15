d84eb134b9287647bf0e4b9cd80bc4b6f6ca2a6c
WeijieChen2017 <weijiechen1994@gmail.com>
2022-04-15 15:39:24 -0500

    Good morning!

--- pre_proc_kspace_2d.py
+++ pre_proc_kspace_2d.py
@@ -10,7 +10,7 @@ train_dict["time_stamp"] = time.strftime("%Y-%m-%d_%H:%M:%S", time.localtime())
 
 train_dict["folder_X"] = "./data_dir/Iman_MR/norm/"
 train_dict["folder_Y"] = "./data_dir/Iman_CT/norm/"
-train_dict["new_modality"] = "kspace_2d_norm"
+train_dict["new_modality"] = "kspace_2d"
 train_dict["old_modality"] = "norm"
 train_dict["norm_MR_mag"] = 400
 train_dict["norm_CT_mag"] = 700
@@ -47,11 +47,10 @@ for cnt_file, file_path in enumerate(X_list):
     x_pack = [x_data, train_dict["norm_MR_mag"]]
     y_pack = [y_data, train_dict["norm_CT_mag"]]
 
-    for pack in [x_pack, y_pack]:
+    for pack in [y_pack]: #x_pack, 
 
         data = pack[0]
         norm_cnst = pack[1]
-
 
         dz = data.shape[2]
         book = np.zeros((dz, num_vocab, cx*cx*2))
@@ -64,24 +63,25 @@ for cnt_file, file_path in enumerate(X_list):
                 for iy in range(ay//cx):
                     patch = data[ix*cx:ix*cx+cx, iy*cx:iy*cx+cx, iz]
                     k_patch = np.fft.fftshift(np.fft.fftn(patch))
-                    k_patch /= norm_cnst
+                    # k_patch /= norm_cnst
                     # print(patch.shape, k_patch.shape, ix*cx, ix*cx+cx, iy*cx, iy*cx+cx)
                     book[iz, cnt_patch, :cx*cx] = np.ravel(k_patch).real
                     book[iz, cnt_patch, cx*cx:] = np.ravel(k_patch).imag
                     cnt_patch += 1
         xy_book.append(book)
 
-    x_book = xy_book[0]
-    y_book = xy_book[1]
+    # x_book = xy_book[0]
+    y_book = xy_book[0]
 
-    x_save_name = x_path.replace(train_dict["old_modality"], train_dict["new_modality"])
+    # x_save_name = x_path.replace(train_dict["old_modality"], train_dict["new_modality"])
     y_save_name = y_path.replace(train_dict["old_modality"], train_dict["new_modality"])
-    x_save_name = x_save_name.replace("nii.gz", "npy")
+    # x_save_name = x_save_name.replace("nii.gz", "npy")
     y_save_name = y_save_name.replace("nii.gz", "npy")
 
-    np.save(x_save_name, x_book)
+    # np.save(x_save_name, x_book)
     np.save(y_save_name, y_book)
-    print(x_save_name, x_book.shape, train_dict["norm_MR_mag"])
+    # print(x_save_name, x_book.shape, train_dict["norm_MR_mag"])
+    print(y_save_name, y_book.shape, train_dict["norm_CT_mag"])
 
 
 
