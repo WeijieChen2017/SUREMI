e5f5a82b53dada2928f5ab6e3a68e23d584d47bd
WeijieChen2017 <weijiechen1994@gmail.com>
2022-04-15 01:03:48 -0500

    Good morning!

--- test_CTGM_2d.py
+++ test_CTGM_2d.py
@@ -128,7 +128,7 @@ for cnt_file, file_path in enumerate(file_list):
     total_file = len(file_list)
     x_path = file_path.replace("MR", "CT")
     # x_path = x_path.replace("kspace_2d", "kspace_2d_e80_S2")
-    y_path = file_path.replace("MR", "CT")
+    y_path = file_path.replace("kspace_2d_e80_S2", "kspace_2d")
     print(x_path, y_path)
     file_name = os.path.basename(file_path)
     print(" ===> [{:03d}]/[{:03d}]: --->".format(cnt_file+1, total_file), file_name, "<---", end="") #
