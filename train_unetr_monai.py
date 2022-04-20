98680f0af60fbd19314d966c42d199d19e547c70
WeijieChen2017 <weijiechen1994@gmail.com>
2022-04-20 13:40:51 -0500

    Good morning!

--- train_unetr_monai.py
+++ train_unetr_monai.py
@@ -106,7 +106,7 @@ model = UNETR(
 #         new_model_state[model_key] = model.state_dict()[model_key]
 
 # model.load_state_dict(new_model_state)
-model = torch.load(train_dict["save_folder"]+"model_best_086.pth", map_location=torch.device('cpu'))
+# model = torch.load(train_dict["save_folder"]+"model_best_086.pth", map_location=torch.device('cpu'))
 
 # model = nn.DataParallel(model)
 model.train()
@@ -240,10 +240,11 @@ for idx_epoch_new in range(train_dict["epochs"]):
             np.save(train_dict["save_folder"]+"npy/Epoch[{:03d}]_Case[{}]_".format(idx_epoch+1, file_name)+iter_tag+"_y.npy", batch_y.cpu().detach().numpy())
             np.save(train_dict["save_folder"]+"npy/Epoch[{:03d}]_Case[{}]_".format(idx_epoch+1, file_name)+iter_tag+"_z.npy", y_hat.cpu().detach().numpy())
 
-            torch.save(model, train_dict["save_folder"]+"model_.pth".format(idx_epoch + 1))
+            # torch.save(model, train_dict["save_folder"]+"model_.pth".format(idx_epoch + 1))
             if np.mean(case_loss) < best_val_loss:
                 # save the best model
                 torch.save(model, train_dict["save_folder"]+"model_best_{:03d}.pth".format(idx_epoch + 1))
+                torch.save(optimizer, train_dict["save_folder"]+"optim_{:03d}.pth".format(idx_epoch + 1))
                 print("Checkpoint saved at Epoch {:03d}".format(idx_epoch + 1))
                 best_val_loss = np.mean(case_loss)
 
