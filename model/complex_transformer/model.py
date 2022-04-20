bb707bfa2e18c20796e7fafdafcd74c7e06e8162
WeijieChen2017 <weijiechen1994@gmail.com>
2022-04-20 00:02:40 -0500

    Good morning!

--- model/complex_transformer/model.py
+++ model/complex_transformer/model.py
@@ -235,6 +235,7 @@ class TransformerGenerationModel(nn.Module):
 
             for i in range(max_len):
                 print(i)
+                print(y_a.size(), y_b.size())
                 dec_a, dec_b = self.trans_decoder(input_A=y_a, input_B=y_b, enc_A=h_as, enc_B=h_bs)
                 # print(y_a.size(), dec_a.unsqueeze(0).size(), y_b.size(), dec_b.unsqueeze(0).size())
                 # y_a, y_b = torch.cat([y_a, dec_a.unsqueeze(0)], dim=0), torch.cat([y_b, dec_b.unsqueeze(0)], dim=0)
