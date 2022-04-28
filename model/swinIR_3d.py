4409cc393604f8ad3c4278208af18712091176e3
WeijieChen2017 <weijiechen1994@gmail.com>
2022-04-27 22:31:22 -0500

    Good morning!

--- model/swinIR_3d.py
+++ model/swinIR_3d.py
@@ -42,6 +42,7 @@ def window_partition(x, window_size):
         windows: (B*num_windows, window_size*window_size*window_size, C)
     """
     B, D, H, W, C = x.shape
+    window_size = to_3tuple(window_size)
     x = x.view(B, D // window_size[0], window_size[0], H // window_size[1], window_size[1], W // window_size[2], window_size[2], C)
     windows = x.permute(0, 1, 3, 5, 2, 4, 6, 7).contiguous().view(-1, reduce(mul, window_size), C)
     return windows
