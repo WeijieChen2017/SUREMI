1686b8634e5bafa9ffc113ad2716d7867ece4869
WeijieChen2017 <weijiechen1994@gmail.com>
2022-04-28 17:23:54 -0500

    Good morning!

--- model/swinIR_3d.py
+++ model/swinIR_3d.py
@@ -878,7 +878,6 @@ class SwinIR3d(nn.Module):
 
     def forward(self, x):
         # B, C, D, H, W
-        print(x.shape)
         D, H, W = x.shape[2:]
         x = self.check_image_size(x)
         
