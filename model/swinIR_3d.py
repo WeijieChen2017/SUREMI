cfa59dfc89f981459c9a82d102fd7dba1d5b92f3
WeijieChen2017 <weijiechen1994@gmail.com>
2022-04-27 22:59:18 -0500

    Good morning!

--- model/swinIR_3d.py
+++ model/swinIR_3d.py
@@ -510,7 +510,6 @@ class RSTB(nn.Module):
             norm_layer=None)
 
     def forward(self, x, x_size):
-        print("513", x.shape, x_size)
         return self.patch_embed(self.conv(self.patch_unembed(self.residual_group(x, x_size), x_size))) + x
 
     def flops(self):
@@ -549,7 +548,7 @@ class PatchEmbed(nn.Module):
         self.in_chans = in_chans
         self.embed_dim = embed_dim
 
-        self.proj = nn.Conv3d(in_chans, embed_dim, kernel_size=patch_size, stride=patch_size)
+        # self.proj = nn.Conv3d(in_chans, embed_dim, kernel_size=patch_size, stride=patch_size)
         if norm_layer is not None:
             self.norm = norm_layer(embed_dim)
         else:
@@ -558,20 +557,24 @@ class PatchEmbed(nn.Module):
     def forward(self, x):
         """Forward function."""
         # padding
-        _, _, D, H, W = x.size()
-        if W % self.patch_size[2] != 0:
-            x = F.pad(x, (0, self.patch_size[2] - W % self.patch_size[2]))
-        if H % self.patch_size[1] != 0:
-            x = F.pad(x, (0, 0, 0, self.patch_size[1] - H % self.patch_size[1]))
-        if D % self.patch_size[0] != 0:
-            x = F.pad(x, (0, 0, 0, 0, 0, self.patch_size[0] - D % self.patch_size[0]))
+        # _, _, D, H, W = x.size()
+        # if W % self.patch_size[2] != 0:
+        #     x = F.pad(x, (0, self.patch_size[2] - W % self.patch_size[2]))
+        # if H % self.patch_size[1] != 0:
+        #     x = F.pad(x, (0, 0, 0, self.patch_size[1] - H % self.patch_size[1]))
+        # if D % self.patch_size[0] != 0:
+        #     x = F.pad(x, (0, 0, 0, 0, 0, self.patch_size[0] - D % self.patch_size[0]))
 
-        x = self.proj(x)  # B C Wd Wh Ww
+        x = x.flatten(2).transpose(1, 2)  # B Ph*Pw C
         if self.norm is not None:
-            Wd, Wh, Ww = x.size(2), x.size(3), x.size(4)
-            x = x.flatten(2).transpose(1, 2)
             x = self.norm(x)
-            x = x.transpose(1, 2).view(-1, self.embed_dim, Wd, Wh, Ww)
+
+        # x = self.proj(x)  # B C Wd Wh Ww
+        # if self.norm is not None:
+        #     Wd, Wh, Ww = x.size(2), x.size(3), x.size(4)
+        #     x = x.flatten(2).transpose(1, 2)
+        #     x = self.norm(x)
+        #     x = x.transpose(1, 2).view(-1, self.embed_dim, Wd, Wh, Ww)
 
         return x
 
