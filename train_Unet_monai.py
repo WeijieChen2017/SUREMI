9fddc175c0ddefa3e23a26a13d06c24aff3c71a9
WeijieChen2017 <weijiechen1994@gmail.com>
2022-05-01 01:30:30 -0500

    Good morning!

--- train_Unet_monai.py
+++ train_Unet_monai.py
@@ -27,7 +27,7 @@ train_dict["seed"] = 426
 train_dict["input_size"] = [96, 96, 96]
 train_dict["gpu_ids"] = [2]
 train_dict["epochs"] = 200
-train_dict["batch"] = 64
+train_dict["batch"] = 32
 train_dict["dropout"] = 0
 train_dict["model_term"] = "Monai_Unet3d"
 
