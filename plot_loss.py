986534250ed4c19b5985ceba5067a01f04642752
WeijieChen2017 <weijiechen1994@gmail.com>
2022-05-06 00:10:36 -0500

    Good morning!

--- plot_loss.py
+++ plot_loss.py
@@ -4,8 +4,8 @@ import glob
 import copy
 import os
 
-n_epoch = 150
-folder = "./project_dir/UnetR_Iman_v2/"
+n_epoch = 12
+folder = "./project_dir/SwinUNETR_Iman_v3_mse/"
 
 stage_hub = []
 npy_list = sorted(glob.glob(folder+"loss/epoch_loss_*.npy"))
