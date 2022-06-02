43c7d7b467388cf8c8006b34be5fa133cd0a74f0
WeijieChen2017 <weijiechen1994@gmail.com>
2022-06-01 19:03:29 -0500

    Good morning!

--- baysian_unet.py
+++ baysian_unet.py
@@ -223,7 +223,7 @@ for idx_epoch_new in range(train_dict["epochs"]):
             np.save(train_dict["save_folder"]+"npy/Epoch[{:03d}]_Case[{}]_".format(idx_epoch+1, file_name)+iter_tag+"_z.npy", y_hat.cpu().detach().numpy())
 
             torch.save(model, train_dict["save_folder"]+"model_.pth".format(idx_epoch + 1))
-            if np.mean(case_loss, axis=1) < best_val_loss:
+            if np.mean(case_loss[:, 1]) < best_val_loss:
                 # save the best model
                 torch.save(model, train_dict["save_folder"]+"model_best_{:03d}.pth".format(idx_epoch + 1))
                 torch.save(optimizer, train_dict["save_folder"]+"optim_{:03d}.pth".format(idx_epoch + 1))
