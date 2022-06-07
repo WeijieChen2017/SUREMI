d38e2ccda424813788ee2cce5f05188310850cce
WeijieChen2017 <weijiechen1994@gmail.com>
2022-06-07 02:26:18 -0500

    Good morning!

--- baysian_unet.py
+++ baysian_unet.py
@@ -59,7 +59,7 @@ class UnetBNN(nn.Module):
 
 train_dict = {}
 train_dict["time_stamp"] = time.strftime("%Y-%m-%d_%H:%M:%S", time.localtime())
-train_dict["project_name"] = "Bayesian_unet_v7_unet_BNN"
+train_dict["project_name"] = "Bayesian_unet_v7_unet_BNN_KLe8"
 train_dict["save_folder"] = "./project_dir/"+train_dict["project_name"]+"/"
 train_dict["seed"] = 426
 # train_dict["input_channel"] = 30
