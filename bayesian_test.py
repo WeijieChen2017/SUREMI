a5ced465edfe520d34b0f95e78b2d87138cf996a
WeijieChen2017 <weijiechen1994@gmail.com>
2022-06-15 20:47:23 -0500

    Good morning!

--- bayesian_test.py
+++ bayesian_test.py
@@ -58,11 +58,11 @@ class UnetBNN(nn.Module):
 test_dict = {}
 test_dict = {}
 test_dict["time_stamp"] = time.strftime("%Y-%m-%d_%H:%M:%S", time.localtime())
-test_dict["project_name"] = "Bayesian_MTGD_v1_unet_do10_MTGD5"
+test_dict["project_name"] = "Bayesian_MTGD_v3_unet_do25_MTGD5"
 test_dict["save_folder"] = "./project_dir/"+test_dict["project_name"]+"/"
 test_dict["gpu_ids"] = [1]
 test_dict["eval_file_cnt"] = 5
-test_dict["best_model_name"] = "model_best_194.pth"
+test_dict["best_model_name"] = "model_best_183.pth"
 test_dict["eval_sample"] = 11
 
 train_dict = np.load(test_dict["save_folder"]+"dict.npy", allow_pickle=True)[()]
