8835885ab6d710bb09747ac9238b9d79a4fe8482
WeijieChen2017 <weijiechen1994@gmail.com>
2022-06-21 16:06:22 -0500

    Good morning!

--- utils/sliding_window_inference.py
+++ utils/sliding_window_inference.py
@@ -201,9 +201,9 @@ def sliding_window_inference(
         seg_mu = np.mean(seg_prob_out_tuple, axis=0)+1e-12
         seg_sigma = np.std(seg_prob_out_tuple, axis=0)
         seg_cov = np.divide(seg_sigma, seg_mu)
-        seg_cov[seg_mu<0.125] = np.nan # -500 is air, (-500+1000)/4000 = 0.125
-        cov_bone_tissue = ma.masked_invalid(seg_cov)
-        cov_array.append(cov_bone_tissue.mean())
+        # seg_cov[seg_mu<0.125] = np.nan # -500 is air, (-500+1000)/4000 = 0.125
+        # cov_bone_tissue = ma.masked_invalid(seg_cov)
+        # cov_array.append(cov_bone_tissue.mean())
         # seg_prob_out = torch.from_numpy(seg_prob_out_median).float().to(device)
         seg_prob_out = torch.from_numpy(seg_cov).float().to(device)
         # >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
