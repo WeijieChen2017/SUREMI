b5540dd069a93b6a86e322a3b77d60f76b95cba0
WeijieChen2017 <weijiechen1994@gmail.com>
2022-06-24 22:11:38 -0500

    Good morning!

--- bayesian_CM.py
+++ bayesian_CM.py
@@ -166,7 +166,7 @@ model = UNet(
     )
 
 print("*"*60)
-test_sample = ( 
+test_sample = (train_dict["batch"], 
                 unet_dict["in_channels"], 
                 train_dict["input_size"][0], 
                 train_dict["input_size"][0], 
