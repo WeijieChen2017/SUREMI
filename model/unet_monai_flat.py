6eb6071e779c07fd68adeb43e98225c3684dbd2a
WeijieChen2017 <weijiechen1994@gmail.com>
2022-06-26 00:58:22 -0500

    Good morning!

--- model/unet_monai_flat.py
+++ model/unet_monai_flat.py
@@ -211,21 +211,21 @@ class UNet_flat(nn.Module):
                 kernel_size=self.kernel_size, subunits=1, act=self.act, norm=self.norm,
                 dropout=self.dropout, bias=self.bias, last_conv_only=False, adn_ordering=self.adn_ordering,))
         self.up3 = nn.Sequential(
-                Convolution(3, self.channels[2]*2, self.channels[1], strides=1,
+                Convolution(3, self.channels[2]*2, self.channels[1], strides=self.strides[2],
                 kernel_size=self.up_kernel_size, act=self.act, norm=self.norm, dropout=self.dropout, 
                 bias=self.bias, conv_only=False, is_transposed=True, adn_ordering=self.adn_ordering),
                 ResidualUnit(3, self.channels[1], self.channels[1], strides=1,
                 kernel_size=self.kernel_size, subunits=1, act=self.act, norm=self.norm,
                 dropout=self.dropout, bias=self.bias, last_conv_only=False, adn_ordering=self.adn_ordering,))
         self.up2 = nn.Sequential(
-                Convolution(3, self.channels[1]*2, self.channels[0], strides=1,
+                Convolution(3, self.channels[1]*2, self.channels[0], strides=self.strides[1],
                 kernel_size=self.up_kernel_size, act=self.act, norm=self.norm, dropout=self.dropout, 
                 bias=self.bias, conv_only=False, is_transposed=True, adn_ordering=self.adn_ordering),
                 ResidualUnit(3, self.channels[0], self.channels[0], strides=1,
                 kernel_size=self.kernel_size, subunits=1, act=self.act, norm=self.norm,
                 dropout=self.dropout, bias=self.bias, last_conv_only=False, adn_ordering=self.adn_ordering,))
         self.up1 = nn.Sequential(
-                Convolution(3, self.channels[0]*2, self.out_channels, strides=1,
+                Convolution(3, self.channels[0]*2, self.out_channels, strides=self.strides[0],
                 kernel_size=self.up_kernel_size, act=self.act, norm=self.norm, dropout=self.dropout, 
                 bias=self.bias, conv_only=False, is_transposed=True, adn_ordering=self.adn_ordering),
                 ResidualUnit(3, self.out_channels, self.out_channels, strides=1,
