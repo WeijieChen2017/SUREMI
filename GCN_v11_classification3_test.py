10b5b2ba45728751623a52ca8f7a433431cae9bb
WeijieChen2017 <weijiechen1994@gmail.com>
2022-07-12 15:15:24 -0500

    Good morning!

--- GCN_v11_classification3_test.py
+++ GCN_v11_classification3_test.py
@@ -182,17 +182,17 @@ for name in name_array:
 
         print(output_data.shape)
 
-        test_file = nib.Nifti1Image(np.squeeze(output_data[0, :, :, :]), x_file.affine, x_file.header)
+        test_file = nib.Nifti1Image(np.squeeze(output_data[:, 0, :, :, :]), x_file.affine, x_file.header)
         test_save_name = train_dict["save_folder"]+test_dict["eval_save_folder"]+"/"+file_name.replace(".nii.gz", "_zair.nii.gz")
         nib.save(test_file, test_save_name)
         print(test_save_name)
 
-        test_file = nib.Nifti1Image(np.squeeze(output_data[1, :, :, :]), x_file.affine, x_file.header)
+        test_file = nib.Nifti1Image(np.squeeze(output_data[:, 1, :, :, :]), x_file.affine, x_file.header)
         test_save_name = train_dict["save_folder"]+test_dict["eval_save_folder"]+"/"+file_name.replace(".nii.gz", "_zsof.nii.gz")
         nib.save(test_file, test_save_name)
         print(test_save_name)
 
-        test_file = nib.Nifti1Image(np.squeeze(output_data[2, :, :, :]), x_file.affine, x_file.header)
+        test_file = nib.Nifti1Image(np.squeeze(output_data[:, 2, :, :, :]), x_file.affine, x_file.header)
         test_save_name = train_dict["save_folder"]+test_dict["eval_save_folder"]+"/"+file_name.replace(".nii.gz", "_zbon.nii.gz")
         nib.save(test_file, test_save_name)
         print(test_save_name)
