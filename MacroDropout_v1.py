5d0ec5fe88893de97c887ab058c1c6cfd6e69d2b
WeijieChen2017 <weijiechen1994@gmail.com>
2022-07-18 02:12:07 -0500

    Good morning!

--- MacroDropout_v1.py
+++ MacroDropout_v1.py
@@ -59,8 +59,8 @@ train_dict["flip"] = False
 
 unet_dict = {}
 unet_dict["spatial_dims"] = 3
-unet_dict["in_channels"] = 5
-unet_dict["out_channels"] = 3
+unet_dict["in_channels"] = 1
+unet_dict["out_channels"] = 1
 unet_dict["channels"] = (32, 64, 128, 256)
 unet_dict["strides"] = (2, 2, 2)
 unet_dict["num_res_units"] = 4
