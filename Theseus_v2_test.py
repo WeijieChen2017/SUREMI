121fd0dca375cf76ca1379e79978245713dba93e
WeijieChen2017 <weijiechen1994@gmail.com>
2022-07-25 00:39:17 -0500

    Good morning!

--- Theseus_v2_test.py
+++ Theseus_v2_test.py
@@ -29,133 +29,144 @@ from utils import iter_all_order
 model_list = [
     "Theseus_v2_181_200_rdp0",
     "Theseus_v2_181_200_rdp1",
-    "Theseus_v2_181_200_rdp10",
-    "Theseus_v2_181_200_rdp100",
+    "Theseus_v2_181_200_rdp20",
+    "Theseus_v2_181_200_rdp40",
+    "Theseus_v2_181_200_rdp60",
+    "Theseus_v2_181_200_rdp80",
 ]
 
 
-for name in model_list:
-    test_dict = {}
-    test_dict = {}
-    test_dict["time_stamp"] = time.strftime("%Y-%m-%d_%H:%M:%S", time.localtime())
-    test_dict["project_name"] = name # "Bayesian_MTGD_v2_unet_do10_MTGD15"
-    test_dict["save_folder"] = "./project_dir/"+test_dict["project_name"]+"/"
-    test_dict["gpu_ids"] = [4]
-    test_dict["eval_file_cnt"] = 5
-    # test_dict["best_model_name"] = "model_best_193.pth"
-    test_dict["eval_sample"] = 128
-    test_dict["eval_save_folder"] = "pred_monai"
+print("Model index: ", end="")
+current_model_idx = int(input()) - 1
+print(model_list[current_model_idx])
+time.sleep(1)
 
-    train_dict = np.load(test_dict["save_folder"]+"dict.npy", allow_pickle=True)[()]
+name = model_list[current_model_idx]
+
+
+# for name in model_list:
+test_dict = {}
+test_dict = {}
+test_dict["time_stamp"] = time.strftime("%Y-%m-%d_%H:%M:%S", time.localtime())
+test_dict["project_name"] = name # "Bayesian_MTGD_v2_unet_do10_MTGD15"
+test_dict["save_folder"] = "./project_dir/"+test_dict["project_name"]+"/"
+test_dict["gpu_ids"] = [4]
+test_dict["eval_file_cnt"] = 10
+# test_dict["best_model_name"] = "model_best_193.pth"
+test_dict["eval_sample"] = 100
+test_dict["eval_save_folder"] = "pred_monai"
+
+train_dict = np.load(test_dict["save_folder"]+"dict.npy", allow_pickle=True)[()]
+
+test_dict["seed"] = train_dict["seed"]
+test_dict["input_size"] = train_dict["input_size"]
+# test_dict["alt_blk_depth"] = train_dict["model_para"]["macro_dropout"]
+test_dict["alt_blk_depth"] = [2,2,2,2,2,2,2]
+
+print("input size:", test_dict["input_size"])
+print("alt_blk_depth", test_dict["alt_blk_depth"])
+
+
+
+for path in [test_dict["save_folder"], test_dict["save_folder"]+test_dict["eval_save_folder"]]:
+    if not os.path.exists(path):
+        os.mkdir(path)
+
+np.save(test_dict["save_folder"]+"test_dict.npy", test_dict)
+
+
+# ==================== basic settings ====================
+
+np.random.seed(test_dict["seed"])
+gpu_list = ','.join(str(x) for x in test_dict["gpu_ids"])
+os.environ['CUDA_VISIBLE_DEVICES'] = gpu_list
+print('export CUDA_VISIBLE_DEVICES=' + gpu_list)
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+model_list = sorted(glob.glob(os.path.join(test_dict["save_folder"], "model_best_*.pth")))
+if "curr" in model_list[-1]:
+    print("Remove model_best_curr")
+    model_list.pop()
+target_model = model_list[-1]
+# target_model = test_dict["save_folder"]+test_dict["best_model_name"]
+model = torch.load(target_model, map_location=torch.device('cpu'))
+print("--->", target_model, " is loaded.")
+
+# ==================== data division ====================
+
+data_div = np.load(os.path.join(test_dict["save_folder"], "data_division.npy"), allow_pickle=True)[()]
+X_list = data_div['test_list_X']
+if test_dict["eval_file_cnt"] > 0:
+    X_list = X_list[:test_dict["eval_file_cnt"]]
+
+
+# ==================== training ====================
+
+file_list = X_list
+iter_tag = "test"
+cnt_total_file = len(file_list)
+cnt_each_cube = 1
+model.eval()
+model = model.to(device)
+
+for cnt_file, file_path in enumerate(file_list):
     
-    test_dict["seed"] = train_dict["seed"]
-    test_dict["input_size"] = train_dict["input_size"]
-    # test_dict["alt_blk_depth"] = train_dict["model_para"]["macro_dropout"]
-    test_dict["alt_blk_depth"] = [2,2,2,2,2,2,2]
+    x_path = file_path
+    y_path = file_path.replace("x", "y")
+    file_name = os.path.basename(file_path)
+    print(iter_tag + " ===> Case[{:03d}/{:03d}]: ".format(cnt_file+1, cnt_total_file), x_path, "<---", end="") # 
+    x_file = nib.load(x_path)
+    y_file = nib.load(y_path)
+    x_data = x_file.get_fdata()
+    y_data = y_file.get_fdata()
 
-    print("input size:", test_dict["input_size"])
-    print("alt_blk_depth", test_dict["alt_blk_depth"])
+    ax, ay, az = x_data.shape
+    case_loss = 0
 
+    input_data = np.expand_dims(x_data, (0,1))
+    input_data = torch.from_numpy(input_data).float().to(device)
 
+    order_list = iter_all_order(test_dict["alt_blk_depth"])
+    order_list = iter_all_order([2,2,2,2,2,2,2,2,2])
+    order_list_cnt = len(order_list)
+    output_array = np.zeros((order_list_cnt, ax, ay, az))
 
-    for path in [test_dict["save_folder"], test_dict["save_folder"]+test_dict["eval_save_folder"]]:
-        if not os.path.exists(path):
-            os.mkdir(path)
+    for idx_es in range(order_list_cnt):
+        with torch.no_grad():
+            # print(order_list[idx_es])
+            y_hat = sliding_window_inference(
+                    inputs = input_data, 
+                    roi_size = test_dict["input_size"], 
+                    sw_batch_size = 8, 
+                    predictor = model,
+                    overlap=0.25, 
+                    mode="gaussian", 
+                    sigma_scale=0.125, 
+                    padding_mode="constant", 
+                    cval=0.0, 
+                    sw_device=device, 
+                    device=device,
+                    # order=order_list[idx_es],
+                    )
+            output_array[idx_es, :, :, :] = y_hat.cpu().detach().numpy()
 
-    np.save(test_dict["save_folder"]+"test_dict.npy", test_dict)
+    output_data = np.median(output_array, axis=0)
+    output_std = np.std(output_array, axis=0)
+    output_mean = np.std(output_array, axis=0)
+    output_cov = np.divide(output_std, output_mean+1e-12)
+    print(output_data.shape)
 
+    test_file = nib.Nifti1Image(np.squeeze(output_data), x_file.affine, x_file.header)
+    test_save_name = train_dict["save_folder"]+test_dict["eval_save_folder"]+"/"+file_name
+    nib.save(test_file, test_save_name)
+    print(test_save_name)
 
-    # ==================== basic settings ====================
+    test_file = nib.Nifti1Image(np.squeeze(output_std), x_file.affine, x_file.header)
+    test_save_name = train_dict["save_folder"]+test_dict["eval_save_folder"]+"/"+file_name.replace(".nii.gz", "_std.nii.gz")
+    nib.save(test_file, test_save_name)
+    print(test_save_name)
 
-    np.random.seed(test_dict["seed"])
-    gpu_list = ','.join(str(x) for x in test_dict["gpu_ids"])
-    os.environ['CUDA_VISIBLE_DEVICES'] = gpu_list
-    print('export CUDA_VISIBLE_DEVICES=' + gpu_list)
-    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-
-    model_list = sorted(glob.glob(os.path.join(test_dict["save_folder"], "model_best_*.pth")))
-    if "curr" in model_list[-1]:
-        print("Remove model_best_curr")
-        model_list.pop()
-    target_model = model_list[-1]
-    # target_model = test_dict["save_folder"]+test_dict["best_model_name"]
-    model = torch.load(target_model, map_location=torch.device('cpu'))
-    print("--->", target_model, " is loaded.")
-
-    # ==================== data division ====================
-
-    data_div = np.load(os.path.join(test_dict["save_folder"], "data_division.npy"), allow_pickle=True)[()]
-    X_list = data_div['test_list_X']
-    if test_dict["eval_file_cnt"] > 0:
-        X_list = X_list[:test_dict["eval_file_cnt"]]
-
-
-    # ==================== training ====================
-
-    file_list = X_list
-    iter_tag = "test"
-    cnt_total_file = len(file_list)
-    cnt_each_cube = 1
-    model.eval()
-    model = model.to(device)
-
-    for cnt_file, file_path in enumerate(file_list):
-        
-        x_path = file_path
-        y_path = file_path.replace("x", "y")
-        file_name = os.path.basename(file_path)
-        print(iter_tag + " ===> Case[{:03d}/{:03d}]: ".format(cnt_file+1, cnt_total_file), x_path, "<---", end="") # 
-        x_file = nib.load(x_path)
-        y_file = nib.load(y_path)
-        x_data = x_file.get_fdata()
-        y_data = y_file.get_fdata()
-
-        ax, ay, az = x_data.shape
-        case_loss = 0
-
-        input_data = np.expand_dims(x_data, (0,1))
-        input_data = torch.from_numpy(input_data).float().to(device)
-
-        order_list = iter_all_order(test_dict["alt_blk_depth"])
-        order_list_cnt = len(order_list)
-        output_array = np.zeros((order_list_cnt, ax, ay, az))
-
-        for idx_es in range(order_list_cnt):
-            with torch.no_grad():
-                print(order_list[idx_es])
-                y_hat = sliding_window_inference(
-                        inputs = input_data, 
-                        roi_size = test_dict["input_size"], 
-                        sw_batch_size = 8, 
-                        predictor = model,
-                        overlap=0.25, 
-                        mode="gaussian", 
-                        sigma_scale=0.125, 
-                        padding_mode="constant", 
-                        cval=0.0, 
-                        sw_device=device, 
-                        device=device,
-                        order=order_list[idx_es],
-                        )
-                output_array[idx_es, :, :, :] = y_hat.cpu().detach().numpy()
-
-        output_data = np.median(output_array, axis=0)
-        output_std = np.std(output_array, axis=0)
-        output_mean = np.std(output_array, axis=0)
-        output_cov = np.divide(output_std, output_mean+1e-12)
-        print(output_data.shape)
-
-        test_file = nib.Nifti1Image(np.squeeze(output_data), x_file.affine, x_file.header)
-        test_save_name = train_dict["save_folder"]+test_dict["eval_save_folder"]+"/"+file_name
-        nib.save(test_file, test_save_name)
-        print(test_save_name)
-
-        test_file = nib.Nifti1Image(np.squeeze(output_std), x_file.affine, x_file.header)
-        test_save_name = train_dict["save_folder"]+test_dict["eval_save_folder"]+"/"+file_name.replace(".nii.gz", "_std.nii.gz")
-        nib.save(test_file, test_save_name)
-        print(test_save_name)
-
-        test_file = nib.Nifti1Image(np.squeeze(output_cov), x_file.affine, x_file.header)
-        test_save_name = train_dict["save_folder"]+test_dict["eval_save_folder"]+"/"+file_name.replace(".nii.gz", "_cov.nii.gz")
-        nib.save(test_file, test_save_name)
-        print(test_save_name)
+    test_file = nib.Nifti1Image(np.squeeze(output_cov), x_file.affine, x_file.header)
+    test_save_name = train_dict["save_folder"]+test_dict["eval_save_folder"]+"/"+file_name.replace(".nii.gz", "_cov.nii.gz")
+    nib.save(test_file, test_save_name)
+    print(test_save_name)
