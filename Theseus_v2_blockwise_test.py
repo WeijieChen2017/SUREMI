dedc35f353be74ed2d403712e6d94c4da748860b
WeijieChen2017 <weijiechen1994@gmail.com>
2022-07-28 03:00:03 -0500

    Good morning!

--- Theseus_v2_blockwise_test.py
+++ Theseus_v2_blockwise_test.py
@@ -50,7 +50,7 @@ time.sleep(1)
 
 item = model_list[current_model_idx]
 name = item[0]
-test_dict["gpu_ids"] = item[1]
+
 
 # for name in model_list:
 test_dict = {}
@@ -58,6 +58,7 @@ test_dict = {}
 test_dict["time_stamp"] = time.strftime("%Y-%m-%d_%H:%M:%S", time.localtime())
 test_dict["project_name"] = name # "Bayesian_MTGD_v2_unet_do10_MTGD15"
 test_dict["save_folder"] = "./project_dir/"+test_dict["project_name"]+"/"
+test_dict["gpu_ids"] = item[1]
 test_dict["eval_file_cnt"] = 0
 test_dict["batch"] = 8
 # test_dict["best_model_name"] = "model_best_193.pth"
