319a5e5aba120198529095c9d2b17c1e0dd9fd12
WeijieChen2017 <weijiechen1994@gmail.com>
2022-08-14 01:03:54 -0500

    Good morning!

--- Theseus_v3_test.py
+++ Theseus_v3_test.py
@@ -32,6 +32,10 @@ model_list = [
     # ["Theseus_v3_channelDO_rdp100", [6], 1.0, False],
     # ["Theseus_v3_channelDOw_rdp050", [7], 0.5, True],
     # ["Theseus_v3_channelDOw_rdp100", [7], 1.0, True],
+    ["Theseus_v4_shuffle_rdp050", [5], 0.5, False],
+    ["Theseus_v4_shuffle_rdp100", [5], 1.0, False],
+    ["Theseus_v4_shuffleW_rdp050", [5], 0.5, True],
+    ["Theseus_v4_shuffleW_rdp100", [5], 1.0, True],
     ["Theseus_v3_channelDOw_rdp050_fixed", [6], 0.5, True],
     ["Theseus_v3_channelDOw_rdp100_fixed", [6], 1.0, True],
 ]
