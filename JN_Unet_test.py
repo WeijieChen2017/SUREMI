b39ba4a07115c353cb88880d296b645068776718
WeijieChen2017 <weijiechen1994@gmail.com>
2022-08-23 12:55:32 -0500

    Good morning!

--- JN_Unet_test.py
+++ JN_Unet_test.py
@@ -12,7 +12,7 @@ if not os.path.exists(train_dict["root_dir"]):
 train_dict["data_dir"] = "./data_dir/JN_BTCV/"
 train_dict["split_JSON"] = "dataset_0.json"
 train_dict["gpu_list"] = [6]
-train_dict["alt_blk_depth"] = [2,2,2,2,2] # [2,2,2,2,2,2,2] for unet
+train_dict["alt_blk_depth"] = [2,2,2,2,2,2,2] # [2,2,2,2,2,2,2] for unet
 # train_dict["alt_blk_depth"] = [2,2,2,2,2,2,2,2,2] # [2,2,2,2,2,2,2,2,2] for unet
 
 import os
@@ -23,6 +23,8 @@ import time
 import random
 
 import numpy as np
+import nibabel as nib
+import torch.nn as nn
 
 from monai.inferers import sliding_window_inference
 
@@ -32,8 +34,6 @@ from monai.data import (
     decollate_batch,
 )
 
-# directory = os.environ.get("./project_dir/JN_UnetR/")
-# root_dir = tempfile.mkdtemp() if directory is None else directory
 root_dir = train_dict["root_dir"]
 print(root_dir)
 
@@ -42,12 +42,6 @@ split_JSON = train_dict["split_JSON"]
 
 datasets = data_dir + split_JSON
 val_files = load_decathlon_datalist(datasets, True, "validation")
-print(val_files)
-
-#--------------------------------------------------------------
-print("Press any key to continue:", end="")
-_ = input()
-#--------------------------------------------------------------
 
 gpu_list = ','.join(str(x) for x in train_dict["gpu_list"])
 os.environ['CUDA_VISIBLE_DEVICES'] = gpu_list
@@ -68,223 +62,57 @@ model = UNet(
     bias=True,
     ).to(device)
 
-# state weights mapping
-# swm_1 = {}
-# swm_1["down1.0"]   = "model.0"
-# swm_1["down2.0"]   = "model.1.submodule.0"
-# swm_1["down3.0"]   = "model.1.submodule.1.submodule.0"
-# swm_1["bottom.0"]  = "model.1.submodule.1.submodule.1.submodule"
-# swm_1["up3.0"]     = "model.1.submodule.1.submodule.2"
-# swm_1["up2.0"]     = "model.1.submodule.2"
-# swm_1["up1.0"]     = "model.2"
-
-# swm_2 = {}
-# swm_1["down1.1"]   = "model.0"
-# swm_1["down2.1"]   = "model.1.submodule.0"
-# swm_1["down3.1"]   = "model.1.submodule.1.submodule.0"
-# swm_1["bottom.1"]  = "model.1.submodule.1.submodule.1.submodule"
-# swm_1["up3.1"]     = "model.1.submodule.1.submodule.2"
-# swm_1["up2.1"]     = "model.1.submodule.2"
-# swm_1["up1.1"]     = "model.2"
-# train_dict["state_weight_mapping_1"] = swm_1
-# train_dict["state_weight_mapping_2"] = swm_2
-# train_dict["target_model_1"] = "./project_dir/JN_Unet/best_metric_model.pth"
-# train_dict["target_model_2"] = "./project_dir/JN_Unet/best_metric_model.pth"
-
-# pretrain_1_state = torch.load(train_dict["target_model_1"])
-# pretrain_2_state = torch.load(train_dict["target_model_2"])
-
-# model_state_keys = model.state_dict().keys()
-# new_model_state = {}
-
 pre_train_state = {}
 pre_train_model = torch.load(train_dict["root_dir"]+"best_metric_model.pth")
 
 for model_key in model.state_dict().keys():
     pre_train_state[model_key] = pre_train_model[model_key]
-    # weight_prefix = model_key[:model_key.find(".")+2]
-
-    # # in the first half
-    # if weight_prefix in swm_1.keys():
-    #     weight_replacement = swm_1[weight_prefix]
-    #     new_model_state[model_key] = pretrain_1_state[model_key.replace(weight_prefix, weight_replacement)]
-
-    # # in the second half
-    # if weight_prefix in swm_2.keys():
-    #     weight_replacement = swm_2[weight_prefix]
-    #     new_model_state[model_key] = pretrain_2_state[model_key.replace(weight_prefix, weight_replacement)]
-    
+     
 model.load_state_dict(pre_train_state)
 
-
-loss_function_rec = DiceCELoss(to_onehot_y=True, softmax=True)
-loss_function_reg = torch.nn.MSELoss()
-torch.backends.cudnn.benchmark = True
-optimizer = torch.optim.AdamW(model.parameters(), lr=1e-4, weight_decay=1e-5)
-
-#--------------------------------------------------------------
-print("Press any key to continue:", end="")
-_ = input()
-#--------------------------------------------------------------
-
-def prediction(epoch_iterator_val):
-    model.eval()
-    order_list = iter_all_order(train_dict["alt_blk_depth"])
-    # order_list = iter_all_order([2,2,2,2,2,2,2,2,2])
-    order_list_cnt = len(order_list)
-    with torch.no_grad():
-        for step, batch in enumerate(epoch_iterator_val):
-            val_inputs, val_labels = (batch["image"].cuda(), batch["label"].cuda())
-            (_, _, ax, ay, az) = val_labels.size() # torch.Size([1, 1, 314, 214, 234])
-            output_array = np.zeros((order_list_cnt, n_cls, ax, ay, az))
-            for idx_bdo in range(order_list_cnt):
-                print(idx_bdo)
-                curr_outputs = sliding_window_inference(
-                    val_inputs, (96, 96, 96), 4, model,
-                    # order=order_list[idx_bdo]
+model.eval()
+order_list = iter_all_order(train_dict["alt_blk_depth"])
+# order_list = iter_all_order([2,2,2,2,2,2,2,2,2])
+order_list_cnt = len(order_list)
+with torch.no_grad():
+    for val_tuple in enumerate(val_files):
+        img_path = val_tuple['image']
+        lab_path = val_tuple['label']
+        file_name = os.path.basename(lab_path)
+        input_data = nib.load(img_path).get_fdata()
+        lab_file = nib.load(lab_path)
+        ax, ay, az = input_data.size()
+        output_array = np.zeros((order_list_cnt, ax, ay, az))
+        for idx_bdo in range(order_list_cnt):
+            print(idx_bdo)
+            y_hat = sliding_window_inference(
+                    inputs = input_data, 
+                    roi_size = (96, 96, 96), 
+                    sw_batch_size = 4, 
+                    predictor = model,
+                    overlap=0.25, 
+                    mode="gaussian", 
+                    sigma_scale=0.125, 
+                    padding_mode="constant", 
+                    cval=0.0, 
+                    sw_device=device, 
+                    device=device,
+                    order=order_list[idx_es],
                     )
-                val_outputs_list = decollate_batch(curr_outputs)
-                val_output_convert = [
-                    post_pred(val_pred_tensor) for val_pred_tensor in val_outputs_list
-                ]
-                output_array[idx_bdo, :, :, :, :] = val_output_convert.cpu().detach().numpy()
+            y_hat = nn.Softmax(dim=1)(y_hat).cpu().detach().numpy()
+            y_hat = np.argmax(np.squeeze(y_hat), axis=0)
+            print(y_hat.shape)
+            output_array[idx_bdo, :, :, :] = y_hat
 
-            val_median = np.median(output_array, axis=0)
-            val_std = np.std(output_array, axis=0)
-            # val_outputs = torch.from_numpy(val_outputs).cuda()
-            # val_outputs = val_outputs.cpu().detach().numpy()
-            print(val_median.shape)
-            np.save(train_dict["root_dir"]+"pred_step_[{:03d}].npy".format(step+1), val_median)
-            np.save(train_dict["root_dir"]+"std_step_[{:03d}].npy".format(step+1), val_std)
-            
-    return 0
-            # val_labels_list = decollate_batch(val_labels)
-            # val_labels_convert = [
-            #     post_label(val_label_tensor) for val_label_tensor in val_labels_list
-            # ]
-            # val_outputs_list = decollate_batch(val_outputs)
-            # val_output_convert = [
-            #     post_pred(val_pred_tensor) for val_pred_tensor in val_outputs_list
-            # ]
-            # dice_metric(y_pred=val_output_convert, y=val_labels_convert)
-            # epoch_iterator_val.set_description(
-            #     "Validate (%d / %d Steps)" % (global_step, 10.0)
-            # )
-            
-        # mean_dice_val = dice_metric.aggregate().item()
-        # dice_metric.reset()
+        val_median = np.median(output_array, axis=0)
+        val_std = np.std(output_array, axis=0)
 
-        
-    # return mean_dice_val
+        test_file = nib.Nifti1Image(np.squeeze(val_median), lab_file.affine, lab_file.header)
+        test_save_name = train_dict["root_dir"]+file_name.replace(".nii.gz", "_pred.nii.gz")
+        nib.save(test_file, test_save_name)
+        print(test_save_name)
 
-def validation(epoch_iterator_val):
-    model.eval()
-    with torch.no_grad():
-        for step, batch in enumerate(epoch_iterator_val):
-            val_inputs, val_labels = (batch["image"].cuda(), batch["label"].cuda())
-            val_outputs = sliding_window_inference(val_inputs, (96, 96, 96), 4, model)
-            val_labels_list = decollate_batch(val_labels)
-            val_labels_convert = [
-                post_label(val_label_tensor) for val_label_tensor in val_labels_list
-            ]
-            val_outputs_list = decollate_batch(val_outputs)
-            val_output_convert = [
-                post_pred(val_pred_tensor) for val_pred_tensor in val_outputs_list
-            ]
-            dice_metric(y_pred=val_output_convert, y=val_labels_convert)
-            epoch_iterator_val.set_description(
-                "Validate (%d / %d Steps)" % (global_step, 10.0)
-            )
-        mean_dice_val = dice_metric.aggregate().item()
-        dice_metric.reset()
-    return mean_dice_val
-
-
-def train(global_step, train_loader, dice_val_best, global_step_best):
-    model.train()
-    epoch_loss = 0
-    step = 0
-    
-    epoch_iterator_val = tqdm(
-        val_loader, desc="Validate (X / X Steps) (dice=X.X)", dynamic_ncols=True
-    )
-    dice_val = prediction(epoch_iterator_val)
-    np.save(train_dict["root_dir"]+"pred_dice_val.npy", dice_val)
-
-    return None
-    # epoch_iterator = tqdm(
-    #     train_loader, desc="Training (X / X Steps) (loss=X.X)", dynamic_ncols=True
-    # )
-    # for step, batch in enumerate(epoch_iterator):
-    #     step += 1
-    #     x, y = (batch["image"].cuda(), batch["label"].cuda())
-    #     logit_map = model(x)
-    #     logit_map_reg = model(x)
-    #     loss = loss_function_rec(logit_map, y)+loss_function_reg(logit_map, logit_map_reg)
-    #     loss.backward()
-    #     epoch_loss += loss.item()
-    #     optimizer.step()
-    #     optimizer.zero_grad()
-    #     epoch_iterator.set_description(
-    #         "Training (%d / %d Steps) (loss=%2.5f)" % (global_step, max_iterations, loss)
-    #     )
-    #     np.save(train_dict["root_dir"]+"step_loss_train_{:03d}.npy".format(step+1), loss.item())
-    #     if (
-    #         global_step % eval_num == 0 and global_step != 0
-    #     ) or global_step == max_iterations:
-    #         epoch_iterator_val = tqdm(
-    #             val_loader, desc="Validate (X / X Steps) (dice=X.X)", dynamic_ncols=True
-    #         )
-    #         dice_val = validation(epoch_iterator_val)
-    #         np.save(train_dict["root_dir"]+"step_loss_cal_{:03d}.npy".format(step+1), dice_val)
-    #         epoch_loss /= step
-    #         epoch_loss_values.append(epoch_loss)
-    #         metric_values.append(dice_val)
-    #         if dice_val > dice_val_best:
-    #             dice_val_best = dice_val
-    #             global_step_best = global_step
-    #             torch.save(
-    #                 model.state_dict(), os.path.join(root_dir, "best_metric_model.pth")
-    #             )
-    #             print(
-    #                 "Model Was Saved ! Current Best Avg. Dice: {} Current Avg. Dice: {}".format(
-    #                     dice_val_best, dice_val
-    #                 )
-    #             )
-    #         else:
-    #             print(
-    #                 "Model Was Not Saved ! Current Best Avg. Dice: {} Current Avg. Dice: {}".format(
-    #                     dice_val_best, dice_val
-    #                 )
-    #             )
-    #     global_step += 1
-    # return global_step, dice_val_best, global_step_best
-
-
-max_iterations = 25000
-eval_num = 500
-post_label = AsDiscrete(to_onehot=14)
-post_pred = AsDiscrete(argmax=True, to_onehot=14)
-dice_metric = DiceMetric(include_background=True, reduction="mean", get_not_nans=False)
-global_step = 0
-dice_val_best = 0.0
-global_step_best = 0
-epoch_loss_values = []
-metric_values = []
-train(global_step, train_loader, dice_val_best, global_step_best)
-# while global_step < max_iterations:
-#     global_step, dice_val_best, global_step_best = train(
-#         global_step, train_loader, dice_val_best, global_step_best
-#     )
-# model.load_state_dict(torch.load(os.path.join(root_dir, "best_metric_model.pth")))
-
-#--------------------------------------------------------------
-# print("Press any key to continue:", end="")
-# _ = input()
-#--------------------------------------------------------------
-
-print(
-    f"train completed, best_metric: {dice_val_best:.4f} "
-    f"at iteration: {global_step_best}"
-)
-
+        test_file = nib.Nifti1Image(np.squeeze(val_std), lab_file.affine, lab_file.header)
+        test_save_name = train_dict["root_dir"]+file_name.replace(".nii.gz", "_std.nii.gz")
+        nib.save(test_file, test_save_name)
+        print(test_save_name)
