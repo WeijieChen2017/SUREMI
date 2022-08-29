546eea6142ce0c7fed0d09ea430b16e78a535068
WeijieChen2017 <weijiechen1994@gmail.com>
2022-08-29 01:36:14 -0500

    Good morning!

--- JN_UnetR_bdo_test_zoom.py
+++ JN_UnetR_bdo_test_zoom.py
@@ -139,7 +139,7 @@ for case_num in range(6):
         for idx_bdo in range(order_list_cnt):
             print(idx_bdo)
             val_outputs = sliding_window_inference(
-                val_inputs, (96, 96, 96), 4, model, overlap=0.8, order=order_list[idx_bdo],
+                val_inputs, (96, 96, 96), 4, model, overlap=0.8, # order=order_list[idx_bdo],
             )
             output_array[idx_bdo, :, :, :] = torch.argmax(val_outputs, dim=1).detach().cpu().numpy()[0, :, :, :]
 
