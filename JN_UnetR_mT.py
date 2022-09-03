e0244b5705f0afb21a6decf5c25c36882663448a
WeijieChen2017 <weijiechen1994@gmail.com>
2022-09-02 23:52:07 -0500

    Good morning!

--- JN_UnetR_mT.py
+++ JN_UnetR_mT.py
@@ -8,7 +8,7 @@ if not os.path.exists(train_dict["root_dir"]):
     os.mkdir(train_dict["root_dir"])
 train_dict["data_dir"] = "./data_dir/JN_BTCV/"
 train_dict["split_JSON"] = "dataset_0.json"
-train_dict["gpu_list"] = [6]
+train_dict["gpu_list"] = [7]
 
 import os
 import gc
