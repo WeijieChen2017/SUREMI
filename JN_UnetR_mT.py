4a6c8a755539b1875cd8c320e62930261c653dbf
WeijieChen2017 <weijiechen1994@gmail.com>
2022-09-02 23:50:31 -0500

    Good morning!

--- JN_UnetR_mT.py
+++ JN_UnetR_mT.py
@@ -2,7 +2,8 @@ import os
 from model import UNETR_mT
 
 train_dict = {}
-train_dict["root_dir"] = "./project_dir/JN_UnetR_mT_4222211111/"
+# train_dict["root_dir"] = "./project_dir/JN_UnetR_mT_4222211111/"
+train_dict["root_dir"] = "./project_dir/JN_UnetR_mT_4111122222/"
 if not os.path.exists(train_dict["root_dir"]):
     os.mkdir(train_dict["root_dir"])
 train_dict["data_dir"] = "./data_dir/JN_BTCV/"
@@ -194,7 +195,8 @@ model = UNETR_mT(
     norm_name="instance",
     res_block=True,
     dropout_rate=0.0,
-    alter_block=[4, 2, 2, 2, 2, 1, 1, 1, 1, 1],
+    # alter_block=[4, 2, 2, 2, 2, 1, 1, 1, 1, 1],
+    alter_block=[4, 1, 1, 1, 1, 2, 2, 2, 2, 2],
 ).to(device)
 
 # state weights mapping
