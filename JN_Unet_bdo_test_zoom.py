603cbdadcf5dde60e269e2cdadb8e04ecbebaf7a
WeijieChen2017 <weijiechen1994@gmail.com>
2022-09-06 04:27:28 -0500

    Good morning!

--- JN_Unet_bdo_test_zoom.py
+++ JN_Unet_bdo_test_zoom.py
@@ -7,7 +7,7 @@ from scipy.stats import mode
 
 # n_cls = 14
 train_dict = {}
-train_dict["root_dir"] = "./project_dir/JN_Unet/"
+train_dict["root_dir"] = "./project_dir/JN_Unet_bdo/"
 if not os.path.exists(train_dict["root_dir"]):
     os.mkdir(train_dict["root_dir"])
 train_dict["data_dir"] = "./data_dir/JN_BTCV/"
@@ -17,8 +17,7 @@ train_dict["gpu_list"] = [6]
 root_dir = train_dict["root_dir"]
 print(root_dir)
 
-train_dict["alt_blk_depth"] = [1]
-# train_dict["alt_blk_depth"] = [2,2,2,2,2,2,2]
+train_dict["alt_blk_depth"] = [2,2,2,2,2,2,2]
 # train_dict["alt_blk_depth"] = [2,2,2,2,2,2,2] # [2,2,2,2,2,2,2] for unet
 # train_dict["alt_blk_depth"] = [2,2,2,2,2,2,2,2,2] # [2,2,2,2,2,2,2,2,2] for unet
 # JN_UnetR_mT_4222211111
@@ -26,7 +25,7 @@ train_dict["alt_blk_depth"] = [1]
 root_dir = train_dict["root_dir"]
 print(root_dir)
 
-order_list, time_frame = iter_some_order(train_dict["alt_blk_depth"], 1)
+order_list, time_frame = iter_some_order(train_dict["alt_blk_depth"], 63)
 order_list_cnt = len(order_list)
 np.save(root_dir+"order_list_"+time_frame+".npy", order_list)
 
@@ -146,7 +145,7 @@ for case_num in range(6):
         for idx_bdo in range(order_list_cnt):
             print(idx_bdo)
             val_outputs = sliding_window_inference(
-                val_inputs, (96, 96, 96), 8, model, overlap=0.25, #order=order_list[idx_bdo],
+                val_inputs, (96, 96, 96), 8, model, overlap=0.25, order=order_list[idx_bdo],
             )
             output_array[:, :, :, idx_bdo] = torch.argmax(val_outputs, dim=1).detach().cpu().numpy()[0, :, :, :]
 
