ab277000903b413638b451488b3a2bf0cdf7fb4e
WeijieChen2017 <weijiechen1994@gmail.com>
2022-09-06 03:48:22 -0500

    Good morning!

--- JN_UnetR_bdo_test_zoom.py
+++ JN_UnetR_bdo_test_zoom.py
@@ -9,14 +9,14 @@ import numpy as np
 
 n_cls = 14
 train_dict = {}
-train_dict["root_dir"] = "./project_dir/JN_UnetR_mT_4222211111/"
+train_dict["root_dir"] = "./project_dir/JN_UnetR_mT_4111122222/"
 if not os.path.exists(train_dict["root_dir"]):
     os.mkdir(train_dict["root_dir"])
 train_dict["data_dir"] = "./data_dir/JN_BTCV/"
 train_dict["split_JSON"] = "dataset_0.json"
 train_dict["gpu_list"] = [7]
-train_dict["alt_blk_depth"] = [4,2,2,2,2,1,1,1,1,1]
-# train_dict["alt_blk_depth"] = [4,1,1,1,1,2,2,2,2,2]
+# train_dict["alt_blk_depth"] = [4,2,2,2,2,1,1,1,1,1]
+train_dict["alt_blk_depth"] = [4,1,1,1,1,2,2,2,2,2]
 # train_dict["alt_blk_depth"] = [2,2,2,2,2,2,2] # [2,2,2,2,2,2,2] for unet
 # train_dict["alt_blk_depth"] = [2,2,2,2,2,2,2,2,2] # [2,2,2,2,2,2,2,2,2] for unet
 # JN_UnetR_mT_4222211111
