25c38ee67e83def2f7efcbbba6aa91f6fe6e92c6
WeijieChen2017 <weijiechen1994@gmail.com>
2022-09-07 13:13:56 -0500

    Good morning!

--- JN_UnetR_bdo_test_zoom.py
+++ JN_UnetR_bdo_test_zoom.py
@@ -9,9 +9,9 @@ from scipy.stats import mode
 import numpy as np
 
 model_list = [
-    ["./project_dir/JN_UnetR_mT_4222211111/",[4,2,2,2,2,1,1,1,1,1], 2],
-    ["./project_dir/JN_UnetR_mT_4111122222/",[4,1,1,1,1,2,2,2,2,2], 2],
-    ["./project_dir/JN_UnetR_bdo/",[2,2,2,2,2,2,2,2,2,2],3]
+    ["./project_dir/JN_UnetR_mT_4222211111/",[4,2,2,2,2,1,1,1,1,1], 6],
+    ["./project_dir/JN_UnetR_mT_4111122222/",[4,1,1,1,1,2,2,2,2,2], 6],
+    ["./project_dir/JN_UnetR_bdo/",[2,2,2,2,2,2,2,2,2,2],7]
 ]
 
 print("Model index: ", end="")
