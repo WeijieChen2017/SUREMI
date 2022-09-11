bdd28aca73514b8d6eaea9654f0797750c2f722e
WeijieChen2017 <weijiechen1994@gmail.com>
2022-09-11 04:34:43 -0500

    Good morning!

--- JN_UnetR_mT.py
+++ JN_UnetR_mT.py
@@ -9,7 +9,7 @@ if not os.path.exists(train_dict["root_dir"]):
 train_dict["data_dir"] = "./data_dir/JN_BTCV/"
 train_dict["split_JSON"] = "dataset_532.json"
 train_dict["gpu_list"] = [6]
-train_dict["alter_block"] = [2,2,2,2,2,2,2,2,2]
+train_dict["alter_block"] = [2,2,2,2,2,2,2,2,2,2]
 train_dict["best_metric_model"] = "Seg532_UnetR"
 
 import os
