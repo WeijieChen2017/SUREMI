490406667dff6ea2c5af7d15c75c6d602a6dd7fb
WeijieChen2017 <weijiechen1994@gmail.com>
2022-09-18 18:05:11 -0500

    Good morning!

--- Theseus_v2_test.py
+++ Theseus_v2_test.py
@@ -139,7 +139,7 @@ for cnt_file, file_path in enumerate(file_list):
     input_data = np.expand_dims(x_data, (0,1))
     input_data = torch.from_numpy(input_data).float().to(device)
 
-    order_list = iter_all_order(test_dict["alt_blk_depth"])
+    order_list, _ = iter_all_order(test_dict["alt_blk_depth"])
     # order_list = iter_all_order([2,2,2,2,2,2,2,2,2])
     order_list_cnt = len(order_list)
     output_array = np.zeros((order_list_cnt, ax, ay, az))
