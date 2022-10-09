3fce99d835575aebf82d038a77149f72c3370c18
WeijieChen2017 <weijiechen1994@gmail.com>
2022-10-09 03:53:59 -0500

    Good morning!

--- Theseus_v6_outlier_test.py
+++ Theseus_v6_outlier_test.py
@@ -170,7 +170,7 @@ for cnt_file, file_path in enumerate(file_list):
             y_hat = sliding_window_inference(
                     inputs = input_data, 
                     roi_size = test_dict["input_size"], 
-                    sw_batch_size = 64, 
+                    sw_batch_size = 8, 
                     predictor = model,
                     overlap=1/test_dict["stride_division"], 
                     mode="gaussian", 
