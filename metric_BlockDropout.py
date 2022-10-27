137cdd5443c93a22f63217f32e94a1c11d19ce0b
WeijieChen2017 <weijiechen1994@gmail.com>
2022-10-27 17:59:35 -0500

    Good morning!

--- metric_BlockDropout.py
+++ metric_BlockDropout.py
@@ -153,64 +153,65 @@ hub_CT_folder = [
 ]
 
 # "rmse", "nrmse", 
-hub_metric = ["mae", "ssim", "psnr", "acutance", 
+hub_metric = ["rmse", "mae", "ssim", "psnr", "acutance", 
               "dice_air", "dice_soft", "dice_bone",
-              "std_air", "std_soft", "std_bone",
-              "mae_air", "mae_soft", "mae_bone",] 
+              # "std_air", "std_soft", "std_bone",
+              # "mae_air", "mae_soft", "mae_bone",] 
 
 
-print("Model index: ", end="")
-current_model_idx = int(input()) - 1
-print(hub_CT_folder[current_model_idx])
-time.sleep(1)
+for idx_model in range(len(hub_CT_folder)):
 
-cnt_CT_folder = current_model_idx
-CT_folder = "./project_dir/"+hub_CT_folder[cnt_CT_folder]+"/pred_monai/"
+    print("Model index: ", end="")
+    current_model_idx = int(input()) - 1
+    print(hub_CT_folder[current_model_idx])
 
-# for cnt_CT_folder, CT_folder in enumerate(hub_CT_folder):
-list_std_folder = sorted(glob.glob(CT_folder+"*_std.nii.gz"))
-cnt_file_CT = len(list_std_folder)
-cnt_metric = len(hub_metric)
+    cnt_CT_folder = current_model_idx
+    CT_folder = "./project_dir/"+hub_CT_folder[cnt_CT_folder]+"/pred_monai/"
 
-table_metric = np.zeros((cnt_file_CT, cnt_metric))
+    # for cnt_CT_folder, CT_folder in enumerate(hub_CT_folder):
+    list_std_folder = sorted(glob.glob(CT_folder+"*_std.nii.gz"))
+    cnt_file_CT = len(list_std_folder)
+    cnt_metric = len(hub_metric)
 
-for cnt_CT, path_std in enumerate(list_std_folder):
-    # print(hub_CT_name[cnt_CT_folder]+" ===> [{:03d}]/[{:03d}]: --->".format(cnt_CT+1, cnt_file_CT), path_std, "<---")
-    path_CT = path_std.replace("_std", "")
-    filename = os.path.basename(path_CT).replace("_xte", "")
-    path_CT_GT = folder_CT_GT+filename
-    print(path_std, path_CT, path_CT_GT)
+    table_metric = np.zeros((cnt_file_CT, cnt_metric))
+
+    for cnt_CT, path_std in enumerate(list_std_folder):
+        # print(hub_CT_name[cnt_CT_folder]+" ===> [{:03d}]/[{:03d}]: --->".format(cnt_CT+1, cnt_file_CT), path_std, "<---")
+        path_CT = path_std.replace("_std", "")
+        filename = os.path.basename(path_CT).replace("_xte", "")
+        path_CT_GT = folder_CT_GT+filename
+        print(path_std, path_CT, path_CT_GT)
 
 
 
-    file_std = nib.load(path_std)
-    file_CT = nib.load(path_CT)
-    file_CT_GT = nib.load(path_CT_GT)
-    data_std = file_std.get_fdata()
-    data_CT = file_CT.get_fdata()
-    data_CT_GT = file_CT_GT.get_fdata()
-    
-    data_x = denorm_CT(data_CT)
-    data_y = denorm_CT(data_CT_GT)
-    
-    # table_metric[cnt_CT, 0] = mean_squared_error(data_x, data_y)
-    # table_metric[cnt_CT, 1] = np.sqrt(mean_squared_error(data_x, data_y))
+        file_std = nib.load(path_std)
+        file_CT = nib.load(path_CT)
+        file_CT_GT = nib.load(path_CT_GT)
+        data_std = file_std.get_fdata()
+        data_CT = file_CT.get_fdata()
+        data_CT_GT = file_CT_GT.get_fdata()
+        
+        data_x = denorm_CT(data_CT)
+        data_y = denorm_CT(data_CT_GT)
+        
+        # table_metric[cnt_CT, 0] = mean_squared_error(data_x, data_y)
+        # table_metric[cnt_CT, 1] = np.sqrt(mean_squared_error(data_x, data_y))
 
-    table_metric[cnt_CT, 0] = rmse(data_x, data_y)
-    table_metric[cnt_CT, 1] = mae(data_x, data_y)
-    table_metric[cnt_CT, 2] = ssim(data_x, data_y, data_range=4000)
-    table_metric[cnt_CT, 3] = psnr(data_x, data_y, data_range=4000)
-    table_metric[cnt_CT, 4] = acutance(data_x)
-    table_metric[cnt_CT, 5] = dice_coe(data_x, data_y, tissue="air")
-    table_metric[cnt_CT, 6] = dice_coe(data_x, data_y, tissue="soft")
-    table_metric[cnt_CT, 7] = dice_coe(data_x, data_y, tissue="bone")
-    # table_metric[cnt_CT, 7] = std_region(data_std, data_y, tissue="air")
-    # table_metric[cnt_CT, 8] = std_region(data_std, data_y, tissue="soft")
-    # table_metric[cnt_CT, 9] = std_region(data_std, data_y, tissue="bone")
-    # table_metric[cnt_CT, 10] = mae_region(data_x, data_y, tissue="air")
-    # table_metric[cnt_CT, 11] = mae_region(data_x, data_y, tissue="soft")
-    # table_metric[cnt_CT, 12] = mae_region(data_x, data_y, tissue="bone")
+        table_metric[cnt_CT, 0] = rmse(data_x, data_y)
+        table_metric[cnt_CT, 1] = mae(data_x, data_y)
+        table_metric[cnt_CT, 2] = ssim(data_x, data_y, data_range=4000)
+        table_metric[cnt_CT, 3] = psnr(data_x, data_y, data_range=4000)
+        table_metric[cnt_CT, 4] = acutance(data_x)
+        table_metric[cnt_CT, 5] = dice_coe(data_x, data_y, tissue="air")
+        table_metric[cnt_CT, 6] = dice_coe(data_x, data_y, tissue="soft")
+        table_metric[cnt_CT, 7] = dice_coe(data_x, data_y, tissue="bone")
+        # table_metric[cnt_CT, 7] = std_region(data_std, data_y, tissue="air")
+        # table_metric[cnt_CT, 8] = std_region(data_std, data_y, tissue="soft")
+        # table_metric[cnt_CT, 9] = std_region(data_std, data_y, tissue="bone")
+        # table_metric[cnt_CT, 10] = mae_region(data_x, data_y, tissue="air")
+        # table_metric[cnt_CT, 11] = mae_region(data_x, data_y, tissue="soft")
+        # table_metric[cnt_CT, 12] = mae_region(data_x, data_y, tissue="bone")
 
-save_name = "./metric_Oct27/"+hub_CT_name[cnt_CT_folder]+"_"+"_".join(hub_metric)+".npy"
-print(save_name)
-np.save(save_name, table_metric)
+    save_name = "./metric_Oct27/"+hub_CT_name[cnt_CT_folder]+"_"+"_".join(hub_metric)+".npy"
+    print(save_name)
+    np.save(save_name, table_metric)
