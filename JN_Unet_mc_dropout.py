ba901f7f37677b60f494bd15823dfe21adec4d8c
WeijieChen2017 <weijiechen1994@gmail.com>
2022-10-30 17:47:50 -0500

    Good morning!

--- JN_Unet_mc_dropout.py
+++ JN_Unet_mc_dropout.py
@@ -6,11 +6,11 @@ from monai.networks.layers.factories import Act, Norm
 # from model import UNETR_bdo as UNETR
 
 model_hub = [
-    ["Seg532_Unet_MC_D25_R100", 0.25, 1, [1]],
-    ["Seg532_Unet_MC_D50_R100", 0.50, 1, [1]],
-    ["Seg532_Unet_MC_D75_R100", 0.75, 1, [0]],
-    ["Seg532_Unet_MC_D50_R010", 0.75, 1, [4]],
-    ["Seg532_Unet_MC_D50_R001", 0.75, 1, [0]],
+    ["Seg532_Unet_MC_D25_R100", 0.25, 1, [0]],
+    ["Seg532_Unet_MC_D50_R100", 0.50, 1, [0]],
+    ["Seg532_Unet_MC_D75_R100", 0.75, 1, [4]],
+    ["Seg532_Unet_MC_D50_R010", 0.75, 1, [5]],
+    ["Seg532_Unet_MC_D50_R001", 0.75, 1, [5]],
 ]
 
 print("Model index: ", end="")
