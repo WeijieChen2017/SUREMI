636e03d0059613bc108122ab6ebc6e9033b08fc4
WeijieChen2017 <weijiechen1994@gmail.com>
2022-11-03 20:53:07 -0500

    Good morning!

--- JN_Unet_mc_dropout.py
+++ JN_Unet_mc_dropout.py
@@ -10,8 +10,8 @@ model_hub = [
     ["Seg532_Unet_MC_D25_R100", 0.25, 1, [5]], #13.5GB
     ["Seg532_Unet_MC_D50_R100", 0.50, 1, [5]], #13.5GB
     ["Seg532_Unet_MC_D75_R100", 0.75, 1, [4]], #13.5GB
-    ["Seg532_Unet_MC_D50_R010", 0.50, 0.1, [5]], #13.5GB
-    ["Seg532_Unet_MC_D50_R001", 0.50, 0.01, [5]], #13.5GB
+    ["Seg532_Unet_MC_D50_R010", 0.50, 0.1, [5]], #13.5GB screen->1
+    ["Seg532_Unet_MC_D50_R001", 0.50, 0.01, [0]], #13.5GB screen->2
 ]
 
 print("Model index: ", end="")
