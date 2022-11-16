e7ba514f4fa56a6d7fc41fbb9f9d1af30966d1a1
WeijieChen2017 <weijiechen1994@gmail.com>
2022-11-16 02:06:02 -0600

    Good morning!

--- Theseus_v7_distillation_pad.py
+++ Theseus_v7_distillation_pad.py
@@ -74,7 +74,7 @@ for model_setting in model_list:
     test_dict["project_name"] = name # "Bayesian_MTGD_v2_unet_do10_MTGD15"
     test_dict["save_folder"] = "./project_dir/"+test_dict["project_name"]+"/"
     test_dict["gpu_ids"] = gpu_list
-    test_dict["eval_file_cnt"] = 1
+    test_dict["eval_file_cnt"] = 5
     # test_dict["best_model_name"] = "model_best_193.pth"
     # test_dict["eval_sample"] = 100
     test_dict["eval_save_folder"] = save_tag
