8dfaf78f3bd667ec071a3ba6906536d759d432f3
WeijieChen2017 <weijiechen1994@gmail.com>
2022-12-05 14:17:29 -0600

    Good morning!

--- JN_dice_coef.py
+++ JN_dice_coef.py
@@ -22,11 +22,11 @@ target_folder = [
 "Seg532_Unet_MC_D50_R010",
 "Seg532_Unet_MC_D50_R100",
 "Seg532_Unet_MC_D75_R100",
-"Seg532_Unet_ab1114444",
-"Seg532_Unet"
-"Seg532_Unet_ab2",
-"Seg532_Unet_ab4",
-"Seg532_Unet_ab4444111",
+# "Seg532_Unet_ab1114444", "pre"
+# "Seg532_Unet" 
+# "Seg532_Unet_ab2", ""
+# "Seg532_Unet_ab4", " pre"
+# "Seg532_Unet_ab4444111", "pre"
 "Seg532_Unet_channnel_r050",
 "Seg532_Unet_channnel_r050w",
 "Seg532_Unet_channnel_r100",
@@ -42,20 +42,21 @@ case_list = [
 "img0031",
 ]
 
-tag = "_RAS_1.5_1.5_2.0_vote"
+tag = "_RAS_1.5_1.5_2.0"
 n_label = 14
-
-workbook = xlsxwriter.Workbook('dice.xlsx')
-# workbook = xlsxwriter.Workbook('synthesis_wilcoxon.xlsx')
-bold = workbook.add_format({'bold': True})
-set_yellow = workbook.add_format({'bg_color': 'yellow'})
-worksheet = workbook.add_worksheet()
 
 # first row
 for idx in range(n_label):
     worksheet.write(0, idx+1, "region_"+str(idx), bold)
 
 for idx_model, model_name in enumerate(target_folder):
+
+    workbook = xlsxwriter.Workbook("./excel/"+model_name+"+dice.xlsx")
+    # workbook = xlsxwriter.Workbook('synthesis_wilcoxon.xlsx')
+    bold = workbook.add_format({'bold': True})
+    set_yellow = workbook.add_format({'bg_color': 'yellow'})
+    worksheet = workbook.add_worksheet()
+
     print(model_name)
     worksheet.write(idx_model+1, 0, model_name, bold)
     IoU = np.zeros((len(case_list), n_label))
@@ -78,5 +79,5 @@ for idx_model, model_name in enumerate(target_folder):
 #     IoU_std = np.std(IoU, axis=0)
     for idx in range(n_label):
         worksheet.write(idx_model+1, idx+1, "{:.4f}".format(IoU_mean[idx]))
-workbook.close()
+    workbook.close()
 
