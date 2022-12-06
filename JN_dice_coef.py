d80ab6dcdf704c763e3865cf936b946fa36cbfd3
WeijieChen2017 <weijiechen1994@gmail.com>
2022-12-05 20:14:19 -0600

    Good morning!

--- JN_dice_coef.py
+++ JN_dice_coef.py
@@ -47,17 +47,17 @@ n_label = 14
 
 for idx_model, model_name in enumerate(target_folder):
 
-    workbook = xlsxwriter.Workbook("./excel/"+model_name+"+dice.xlsx")
-    # workbook = xlsxwriter.Workbook('synthesis_wilcoxon.xlsx')
-    bold = workbook.add_format({'bold': True})
-    set_yellow = workbook.add_format({'bg_color': 'yellow'})
-    worksheet = workbook.add_worksheet()# first row
-    for idx in range(n_label):
-        worksheet.write(0, idx+1, "region_"+str(idx), bold)
+    # workbook = xlsxwriter.Workbook("./excel/"+model_name+"+dice.xlsx")
+    # # workbook = xlsxwriter.Workbook('synthesis_wilcoxon.xlsx')
+    # bold = workbook.add_format({'bold': True})
+    # set_yellow = workbook.add_format({'bg_color': 'yellow'})
+    # worksheet = workbook.add_worksheet()# first row
+    # for idx in range(n_label):
+    #     worksheet.write(0, idx+1, "region_"+str(idx), bold)
 
-    print(model_name)
-    worksheet.write(idx_model+1, 0, model_name, bold)
-    IoU = np.zeros((len(case_list), n_label))
+    # print(model_name)
+    # worksheet.write(idx_model+1, 0, model_name, bold)
+    # IoU = np.zeros((len(case_list), n_label))
     for idx_case, case_name in enumerate(case_list):
         # Seg532_Unet_ab2_img0026_y_RAS_1.5_1.5_2.0.npy
         y_path = "./moved_files_Nov5/"+model_name+"_"+case_name+"_y"+tag+".npy"
@@ -67,15 +67,17 @@ for idx_model, model_name in enumerate(target_folder):
         y_data = np.load(y_path)
         z_data = np.load(z_path)
         CM = confusion_matrix(np.ravel(y_data), np.ravel(z_data))
-        CM_y = np.sum(CM, axis=0)
-        CM_z = np.sum(CM, axis=1)
-        for i in range(n_label):
-            intersection = CM[i, i]
-            union = CM_y[i]+CM_z[i]
-            IoU[idx_case, i] = 2*intersection/union
-    IoU_mean = np.mean(IoU, axis=0)
-#     IoU_std = np.std(IoU, axis=0)
-    for idx in range(n_label):
-        worksheet.write(idx_model+1, idx+1, "{:.4f}".format(IoU_mean[idx]))
-    workbook.close()
+        save_name = "./excel/"+model_name+"_"+case_name+"_CM.npy"
+        np.save(save_name, CM)
+#         CM_y = np.sum(CM, axis=0)
+#         CM_z = np.sum(CM, axis=1)
+#         for i in range(n_label):
+#             intersection = CM[i, i]
+#             union = CM_y[i]+CM_z[i]
+#             IoU[idx_case, i] = 2*intersection/union
+#     IoU_mean = np.mean(IoU, axis=0)
+# #     IoU_std = np.std(IoU, axis=0)
+#     for idx in range(n_label):
+#         worksheet.write(idx_model+1, idx+1, "{:.4f}".format(IoU_mean[idx]))
+#     workbook.close()
 
