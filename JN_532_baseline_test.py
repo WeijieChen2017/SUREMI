0435e096ff01f8493f003f3f7cae43f42957e511
WeijieChen2017 <weijiechen1994@gmail.com>
2023-01-23 21:50:15 -0600

    Good morning!

--- JN_532_baseline_test.py
+++ JN_532_baseline_test.py
@@ -5,18 +5,6 @@ from monai.networks.layers.factories import Act, Norm
 from utils import iter_all_order
 from scipy.stats import mode
 
-n_cls = 14
-train_dict = {}
-train_dict["root_dir"] = "./project_dir/Seg532_Unet/"
-# train_dict["root_dir"] = "./project_dir/Seg532_UnetR/"
-if not os.path.exists(train_dict["root_dir"]):
-    os.mkdir(train_dict["root_dir"])
-train_dict["data_dir"] = "./data_dir/JN_BTCV/"
-train_dict["split_JSON"] = "dataset_532.json"
-train_dict["gpu_list"] = [6]
-train_dict["alt_blk_depth"] = [2,2,2,2,2,2,2] # [2,2,2,2,2,2,2] for unet
-# train_dict["alt_blk_depth"] = [2,2,2,2,2,2,2,2,2] # [2,2,2,2,2,2,2,2,2] for unet
-
 
 import os
 import shutil
@@ -56,150 +44,218 @@ from monai.data import (
 
 import torch
 
-root_dir = train_dict["root_dir"]
-print(root_dir)
+n_cls = 14
+train_dict = {}
+model_group = [
+[
+    [1001, [2],],
+    [1002, [2],],
+    [1003, [2],],
+    [1004, [2],],
+    [1005, [2],],
+    [1006, [2],],
+    [1007, [2],],
+    [1008, [2],],
+],
+[
+    [1009, [2],],
+    [1010, [2],],
+    [1011, [2],],
+    [1012, [2],],
+    [1013, [2],],
+    [1013, [2],],
+    [1014, [2],],
+    [1015, [2],],
+],
+[
+    [1016, [3],],
+    [1017, [3],],
+    [1018, [3],],
+    [1019, [3],],
+    [1020, [3],],
+    [1021, [3],],
+    [1022, [3],],
+    [1023, [3],],
+],
+[
+    [1024, [3],],
+    [1025, [3],],
+    [1026, [3],],
+    [1027, [3],],
+    [1028, [3],],
+    [1029, [3],],
+    [1030, [3],],
+    [1031, [3],],
+    [1032, [3],],
+]
+]
 
-data_dir = train_dict["data_dir"]
-split_JSON = train_dict["split_JSON"]
+print("Model group index: ", end="")
+current_model_group_idx = int(input()) - 1
+print(model_group[current_model_group_idx])
+time.sleep(1)
 
-datasets = data_dir + split_JSON
-val_files = load_decathlon_datalist(datasets, True, "test")
+model_list = model_group[current_model_group_idx]
 
-gpu_list = ','.join(str(x) for x in train_dict["gpu_list"])
-os.environ['CUDA_VISIBLE_DEVICES'] = gpu_list
-print('export CUDA_VISIBLE_DEVICES=' + gpu_list)
-os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+for idx_sub_group in range(8):
+    seed = model_list[idx_sub_group][0]
+    folder_name = str(seed)
+    gpu = model_list[idx_sub_group][1]
+    # Seg532_Unet_seed1016
+    train_dict["root_dir"] = "./project_dir/Seg532_Unet_seed"+folder_name+"/"
+    # if not os.path.exists(train_dict["root_dir"]):
+    #     os.mkdir(train_dict["root_dir"])
+    train_dict["data_dir"] = "./data_dir/JN_BTCV/"
+    train_dict["split_JSON"] = "dataset_532.json"
+    train_dict["gpu_list"] = gpu
+    train_dict["alt_blk_depth"] = [1]
+    # train_dict["alt_blk_depth"] = [2,2,2,2,2,2,2] # [2,2,2,2,2,2,2] for unet
+    # train_dict["alt_blk_depth"] = [2,2,2,2,2,2,2,2,2] # [2,2,2,2,2,2,2,2,2] for unet
 
+    root_dir = train_dict["root_dir"]
+    print(root_dir)
 
-val_transforms = Compose(
-    [
-        LoadImaged(keys=["image", "label"]),
-        EnsureChannelFirstd(keys=["image", "label"]),
-        Orientationd(keys=["image", "label"], axcodes="RAS"),
-        Spacingd(
-            keys=["image", "label"],
-            pixdim=(1.5, 1.5, 2.0),
-            mode=("bilinear", "nearest"),
-        ),
-        ScaleIntensityRanged(
-            keys=["image"], a_min=-175, a_max=250, b_min=0.0, b_max=1.0, clip=True
-        ),
-        CropForegroundd(keys=["image", "label"], source_key="image"),
-    ]
-)
+    data_dir = train_dict["data_dir"]
+    split_JSON = train_dict["split_JSON"]
 
-datasets = data_dir + split_JSON
-datalist = load_decathlon_datalist(datasets, True, "training")
-val_files = load_decathlon_datalist(datasets, True, "test")
+    datasets = data_dir + split_JSON
+    val_files = load_decathlon_datalist(datasets, True, "test")
 
-val_ds = CacheDataset(
-    data=val_files, transform=val_transforms, cache_num=6, cache_rate=1.0, num_workers=4
-)
-val_loader = DataLoader(
-    val_ds, batch_size=1, shuffle=False, num_workers=4, pin_memory=True
-)
-
-model = UNet( 
-    spatial_dims=3,
-    in_channels=1,
-    out_channels=14,
-    channels=(64, 128, 256, 512),
-    strides=(2, 2, 2),
-    num_res_units=6,
-    act=Act.PRELU,
-    norm=Norm.INSTANCE,
-    dropout=0.,
-    bias=True,
-    ).to(device)#.to(device).load_state_dict(torch.load(os.path.join(root_dir, "best_metric_model.pth")))
-
-pre_train_state = {}
-pre_train_model = torch.load(train_dict["root_dir"]+"best_metric_model.pth")
-
-for model_key in model.state_dict().keys():
-    pre_train_state[model_key] = pre_train_model[model_key]
-     
-model.load_state_dict(pre_train_state)
-
-# model.eval()
-model.train()
-order_list = iter_all_order(train_dict["alt_blk_depth"])
-# order_list = iter_all_order([2,2,2,2,2,2,2,2,2])
-order_list_cnt = len(order_list)
+    gpu_list = ','.join(str(x) for x in train_dict["gpu_list"])
+    os.environ['CUDA_VISIBLE_DEVICES'] = gpu_list
+    print('export CUDA_VISIBLE_DEVICES=' + gpu_list)
+    os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 
-for case_num in range(6):
-    # case_num = 4
-    # model.eval()
-    with torch.no_grad():
-        img_name = os.path.split(val_ds[case_num]["image_meta_dict"]["filename_or_obj"])[1]
-        img = val_ds[case_num]["image"]
-        label = val_ds[case_num]["label"]
-        val_inputs = torch.from_numpy(np.expand_dims(img, 1)).float().cuda()
-        val_labels = torch.from_numpy(np.expand_dims(label, 1)).float().cuda()
+    val_transforms = Compose(
+        [
+            LoadImaged(keys=["image", "label"]),
+            EnsureChannelFirstd(keys=["image", "label"]),
+            Orientationd(keys=["image", "label"], axcodes="RAS"),
+            Spacingd(
+                keys=["image", "label"],
+                pixdim=(1.5, 1.5, 2.0),
+                mode=("bilinear", "nearest"),
+            ),
+            ScaleIntensityRanged(
+                keys=["image"], a_min=-175, a_max=250, b_min=0.0, b_max=1.0, clip=True
+            ),
+            CropForegroundd(keys=["image", "label"], source_key="image"),
+        ]
+    )
 
-        _, _, ax, ay, az = val_labels.size()
-        total_pixel = ax * ay * az
-        output_array = np.zeros((ax, ay, az, order_list_cnt))
-        for idx_bdo in range(order_list_cnt):
-            # print(idx_bdo)
-            # print(device)
-            val_outputs = sliding_window_inference(
-                val_inputs, [96, 96, 96], 8, model, overlap=1/8, device=device,
-                mode="gaussian", sigma_scale=0.125, padding_mode="constant", # , order=order_list[idx_bdo],
+    datasets = data_dir + split_JSON
+    datalist = load_decathlon_datalist(datasets, True, "training")
+    val_files = load_decathlon_datalist(datasets, True, "test")
+
+    val_ds = CacheDataset(
+        data=val_files, transform=val_transforms, cache_num=6, cache_rate=1.0, num_workers=4
+    )
+    val_loader = DataLoader(
+        val_ds, batch_size=1, shuffle=False, num_workers=4, pin_memory=True
+    )
+
+    model = UNet( 
+        spatial_dims=3,
+        in_channels=1,
+        out_channels=14,
+        channels=(64, 128, 256, 512),
+        strides=(2, 2, 2),
+        num_res_units=6,
+        act=Act.PRELU,
+        norm=Norm.INSTANCE,
+        dropout=0.,
+        bias=True,
+        ).to(device)#.to(device).load_state_dict(torch.load(os.path.join(root_dir, "best_metric_model.pth")))
+
+    pre_train_state = {}
+    pre_train_model = torch.load(train_dict["root_dir"]+"best_metric_model.pth")
+
+    for model_key in model.state_dict().keys():
+        pre_train_state[model_key] = pre_train_model[model_key]
+         
+    model.load_state_dict(pre_train_state)
+
+    model.eval()
+    # model.train()
+    order_list = iter_all_order(train_dict["alt_blk_depth"])
+    # order_list = iter_all_order([2,2,2,2,2,2,2,2,2])
+    order_list_cnt = len(order_list)
+
+
+    for case_num in range(6):
+        # case_num = 4
+        # model.eval()
+        with torch.no_grad():
+            img_name = os.path.split(val_ds[case_num]["image_meta_dict"]["filename_or_obj"])[1]
+            img = val_ds[case_num]["image"]
+            label = val_ds[case_num]["label"]
+            val_inputs = torch.from_numpy(np.expand_dims(img, 1)).float().cuda()
+            val_labels = torch.from_numpy(np.expand_dims(label, 1)).float().cuda()
+
+            _, _, ax, ay, az = val_labels.size()
+            total_pixel = ax * ay * az
+            output_array = np.zeros((ax, ay, az, order_list_cnt))
+            for idx_bdo in range(order_list_cnt):
+                # print(idx_bdo)
+                # print(device)
+                val_outputs = sliding_window_inference(
+                    val_inputs, [96, 96, 96], 8, model, overlap=1/8, device=device,
+                    mode="gaussian", sigma_scale=0.125, padding_mode="constant", # , order=order_list[idx_bdo],
+                )
+                output_array[:, :, :, idx_bdo] = torch.argmax(val_outputs, dim=1).detach().cpu().numpy()[0, :, :, :]
+
+            val_mode = np.asarray(np.squeeze(mode(output_array, axis=3).mode), dtype=int)
+
+            # for idx_diff in range(order_list_cnt):
+            #     output_array[:, :, :, idx_diff] -= val_mode
+            # output_array = np.abs(output_array)
+            # output_array[output_array>0] = 1
+
+            # val_pct = np.sum(output_array, axis=3)/order_list_cnt
+
+
+            # vote recorder
+            # 128 list * 128 error
+            # for idx_vote in range(order_list_cnt):
+            #     curr_path = order_list[idx_vote]
+            #     curr_error = np.sum(output_array[:, :, :, idx_vote])/total_pixel
+            #     for idx_path in range(len(train_dict["alt_blk_depth"])):
+            #         # e.g. [*,*,1,*,*] then errors go to this list
+            #         path_vote[idx_path][order_list[idx_vote][idx_path]].append(curr_error)
+
+            # np.save(
+            #     train_dict["root_dir"]+img_name.replace(".nii.gz", "_vote.npy"), 
+            #     path_vote,
+            # )
+            # print(train_dict["root_dir"]+img_name.replace(".nii.gz", "_vote.npy"))
+
+
+
+            np.save(
+                train_dict["root_dir"]+img_name.replace(".nii.gz", "_x_RAS_1.5_1.5_2.0_vote.npy"), 
+                val_inputs.cpu().numpy()[0, 0, :, :, :],
             )
-            output_array[:, :, :, idx_bdo] = torch.argmax(val_outputs, dim=1).detach().cpu().numpy()[0, :, :, :]
+            print(train_dict["root_dir"]+img_name.replace(".nii.gz", "_x_RAS_1.5_1.5_2.0_vote.npy"))
 
-        val_mode = np.asarray(np.squeeze(mode(output_array, axis=3).mode), dtype=int)
+            np.save(
+                train_dict["root_dir"]+img_name.replace(".nii.gz", "_y_RAS_1.5_1.5_2.0_vote.npy"), 
+                val_labels.cpu().numpy()[0, 0, :, :, :],
+            )
+            print(train_dict["root_dir"]+img_name.replace(".nii.gz", "_y_RAS_1.5_1.5_2.0_vote.npy"))
 
-        for idx_diff in range(order_list_cnt):
-            output_array[:, :, :, idx_diff] -= val_mode
-        output_array = np.abs(output_array)
-        output_array[output_array>0] = 1
+            np.save(
+                train_dict["root_dir"]+img_name.replace(".nii.gz", "_z_RAS_1.5_1.5_2.0_vote.npy"), 
+                val_mode,
+            )
+            print(train_dict["root_dir"]+img_name.replace(".nii.gz", "_z_RAS_1.5_1.5_2.0_vote.npy"))
 
-        val_pct = np.sum(output_array, axis=3)/order_list_cnt
-
-
-        # vote recorder
-        # 128 list * 128 error
-        # for idx_vote in range(order_list_cnt):
-        #     curr_path = order_list[idx_vote]
-        #     curr_error = np.sum(output_array[:, :, :, idx_vote])/total_pixel
-        #     for idx_path in range(len(train_dict["alt_blk_depth"])):
-        #         # e.g. [*,*,1,*,*] then errors go to this list
-        #         path_vote[idx_path][order_list[idx_vote][idx_path]].append(curr_error)
-
-        # np.save(
-        #     train_dict["root_dir"]+img_name.replace(".nii.gz", "_vote.npy"), 
-        #     path_vote,
-        # )
-        # print(train_dict["root_dir"]+img_name.replace(".nii.gz", "_vote.npy"))
-
-
-
-        np.save(
-            train_dict["root_dir"]+img_name.replace(".nii.gz", "_x_RAS_1.5_1.5_2.0_vote.npy"), 
-            val_inputs.cpu().numpy()[0, 0, :, :, :],
-        )
-        print(train_dict["root_dir"]+img_name.replace(".nii.gz", "_x_RAS_1.5_1.5_2.0_vote.npy"))
-
-        np.save(
-            train_dict["root_dir"]+img_name.replace(".nii.gz", "_y_RAS_1.5_1.5_2.0_vote.npy"), 
-            val_labels.cpu().numpy()[0, 0, :, :, :],
-        )
-        print(train_dict["root_dir"]+img_name.replace(".nii.gz", "_y_RAS_1.5_1.5_2.0_vote.npy"))
-
-        np.save(
-            train_dict["root_dir"]+img_name.replace(".nii.gz", "_z_RAS_1.5_1.5_2.0_vote.npy"), 
-            val_mode,
-        )
-        print(train_dict["root_dir"]+img_name.replace(".nii.gz", "_z_RAS_1.5_1.5_2.0_vote.npy"))
-
-        np.save(
-            train_dict["root_dir"]+img_name.replace(".nii.gz", "_pct_RAS_1.5_1.5_2.0_vote.npy"), 
-            val_pct,
-        )
-        print(train_dict["root_dir"]+img_name.replace(".nii.gz", "_pct_RAS_1.5_1.5_2.0_vote.npy"))
+            # np.save(
+            #     train_dict["root_dir"]+img_name.replace(".nii.gz", "_pct_RAS_1.5_1.5_2.0_vote.npy"), 
+            #     val_pct,
+            # )
+            # print(train_dict["root_dir"]+img_name.replace(".nii.gz", "_pct_RAS_1.5_1.5_2.0_vote.npy"))
 
 
 
