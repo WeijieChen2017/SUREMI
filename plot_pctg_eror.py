7292dd8037ff4d89985e9a5cac69fc5d11f68971
WeijieChen2017 <weijiechen1994@gmail.com>
2023-03-22 20:04:22 -0500

    Good morning!

--- plot_pctg_eror.py
+++ plot_pctg_eror.py
@@ -74,11 +74,11 @@ for model_name in model_name_list:
     pctg_eror_plot = np.zeros((2, len(pctg_eror_stat.keys())))
     for idx, key in enumerate(pctg_eror_stat.keys()):
         pctg_eror_plot[0, idx] = key
-        if len(pctg_eror_stat[key]) == 1:
+        if len(pctg_eror_stat[key]) == 2:
             pctg_total = np.array(pctg_eror_stat[key])
         else:
             pctg_total = np.sum(np.array(pctg_eror_stat[key]), axis=0)
-        print(len(pctg_eror_stat[key]), pctg_total)
+        # print(len(pctg_eror_stat[key]), pctg_total)
         pctg_eror_plot[1, idx] = pctg_total[1] / np.sum(pctg_total)
     save_name = "./" + folder_name + "/stat_" + model_name + "_pctg_eror.npy"
     np.save(save_name, pctg_eror_plot)
