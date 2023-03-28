9e4552ab099130c1d1be7d50aaf32e6b6cd58331
WeijieChen2017 <weijiechen1994@gmail.com>
2023-03-28 16:55:12 -0500

    Good morning!

--- Theseus_v10_lrn4.py
+++ Theseus_v10_lrn4.py
@@ -62,7 +62,7 @@ train_dict["save_folder"] = "./project_dir/"+train_dict["project_name"]+"/"
 train_dict["seed"] = 426
 train_dict["input_size"] = [96, 96, 96]
 train_dict["epochs"] = 400
-train_dict["batch"] = 16
+train_dict["batch"] = 4
 train_dict["target_model"] = "./project_dir/Unet_Monai_Iman_v2/model_best_181.pth"
 
 train_dict["model_term"] = "Monai_Unet_MacroDropout"
