9c73554d9123fda7a38d48d80e7476c847689c29
WeijieChen2017 <weijiechen1994@gmail.com>
2023-04-04 03:11:58 -0500

    Good morning!

--- test_metric.py
+++ test_metric.py
@@ -69,7 +69,11 @@ hub_CT_name = [
     # "lrn4_444p",
     # "lrn4_441p",
     # "lrn4_144p",
-    "lrn4_222p",
+    # "lrn4_222p",
+    # "lrn4_444f",
+    "lrn4_441f",
+    # "lrn4_144f",
+    # "lrn4_222f",
     ]
 hub_CT_folder = [
     # "./project_dir/SwinUNETR_Iman_v4_mse/pred_monai/",
@@ -78,9 +82,9 @@ hub_CT_folder = [
     # "./project_dir/syn_DLE_1114444_e400_lrn4/full_val/",
     # "./project_dir/syn_DLE_2222222_e400_lrn4/full_val/",
     # "./project_dir/syn_DLE_4444444_e400_lrn4/part_val/",
-    # "./project_dir/syn_DLE_4444111_e400_lrn4/part_val/",
+    "./project_dir/syn_DLE_4444111_e400_lrn4/part_val/",
     # "./project_dir/syn_DLE_1114444_e400_lrn4/part_val/",
-    "./project_dir/syn_DLE_2222222_e400_lrn4/part_val/",
+    # "./project_dir/syn_DLE_2222222_e400_lrn4/part_val/",
 ]
 
 hub_metric = ["rmse", "nrmse", "mae", "ssim", "psnr", "acutance", 
