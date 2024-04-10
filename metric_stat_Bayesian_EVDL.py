7122010e34c5697ed0a655efc7026212d7d11767
WeijieChen2017 <weijiechen1994@gmail.com>
2024-04-10 11:31:54 -0500

    Good morning!

--- metric_stat_Bayesian_EVDL.py
+++ metric_stat_Bayesian_EVDL.py
@@ -10,12 +10,17 @@ error_bay_corr = []
 error_evdl_corr = []
 
 for result_file in results_list:
+    print("Loading", result_file)
     result = np.load(result_file, allow_pickle=True).item()
     #     data = {
     #     "error_std_corr": error_std_corr,
     #     "error_bay_corr": error_bay_corr,
     #     "error_evdl_corr": error_evdl_corr
     # }
+    print("error_std_corr", result["error_std_corr"].shape)
+    print("error_bay_corr", result["error_bay_corr"].shape)
+    print("error_evdl_corr", result["error_evdl_corr"].shape)
+    print()
     error_std_corr.append(result["error_std_corr"])
     error_bay_corr.append(result["error_bay_corr"])
     error_evdl_corr.append(result["error_evdl_corr"])
@@ -23,6 +28,10 @@ for result_file in results_list:
 error_std_corr = np.mean(np.array(error_std_corr))
 error_bay_corr = np.mean(np.array(error_bay_corr))
 error_evdl_corr = np.mean(np.array(error_evdl_corr))
+print("Loaded all results")
+print("error_std_corr", error_std_corr.shape)
+print("error_bay_corr", error_bay_corr.shape)
+print("error_evdl_corr", error_evdl_corr.shape)
 
 corr_dict = {
     "error_std_corr": error_std_corr,
