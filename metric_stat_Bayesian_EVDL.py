b3b7a8694a1f4adb9931f1bd4ebe3fa9a3e6c4f8
WeijieChen2017 <weijiechen1994@gmail.com>
2024-04-16 16:38:26 -0500

    Good morning!

--- metric_stat_Bayesian_EVDL.py
+++ metric_stat_Bayesian_EVDL.py
@@ -3,7 +3,7 @@ import glob
 import os
 
 results_folder = "results/Bayesian_EVDL"
-results_list = sorted(glob.glob(os.path.join(results_folder, "*.nii.gz.npy")))
+results_list = sorted(glob.glob(os.path.join(results_folder, "*.npy")))
 
 error_std_corr = []
 error_bay_corr = []
@@ -25,9 +25,9 @@ for result_file in results_list:
     error_bay_corr.append(result["error_bay_corr"])
     error_evdl_corr.append(result["error_evdl_corr"])
 
-error_std_corr = np.mean(np.array(error_std_corr))
-error_bay_corr = np.mean(np.array(error_bay_corr))
-error_evdl_corr = np.mean(np.array(error_evdl_corr))
+error_std_corr = np.sum(np.array(error_std_corr))
+error_bay_corr = np.sum(np.array(error_bay_corr))
+error_evdl_corr = np.sum(np.array(error_evdl_corr))
 print("Loaded all results")
 print("error_std_corr", error_std_corr.shape)
 print("error_bay_corr", error_bay_corr.shape)
