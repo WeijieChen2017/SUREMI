f62654d9948546e02d1d3b5bd6fcf76cf73b73e7
WeijieChen2017 <weijiechen1994@gmail.com>
2024-04-19 10:53:22 -0500

    Good morning!

---
+++ metric_plot_two_masks.py
@@ -0,0 +1,14 @@
+# load two masks and plot them side by side
+import os
+import glob
+import numpy as np
+import nibabel as nib
+
+data_folder = "results/dice_iou/"
+case_list = sorted(glob.glob(f"{data_folder}/*/*.mask.nii.gz"))
+model_id_list = []
+for case in case_list:
+    model_id = case.split("/")[1]
+    if model_id not in model_id_list:
+        model_id_list.append(model_id)
+print(model_id_list)
