c6bafedf08d0e4d03e81260af23808c81f3bc06a
WeijieChen2017 <weijiechen1994@gmail.com>
2024-04-19 10:56:57 -0500

    Good morning!

--- metric_plot_two_masks.py
+++ metric_plot_two_masks.py
@@ -8,7 +8,7 @@ data_folder = "results/dice_iou/"
 case_list = sorted(glob.glob(f"{data_folder}/*/*mask*.nii.gz"))
 model_id_list = []
 for case in case_list:
-    model_id = case.split("/")[1]
+    model_id = case.split("/")[2]
     if model_id not in model_id_list:
         model_id_list.append(model_id)
 print(model_id_list)
